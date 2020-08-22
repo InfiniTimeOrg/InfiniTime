ecbbeb6283178696ae36891225e086d3154e8a8a
JF <jf@codingfield.com>
2020-08-22 16:42:53 +0200

    Fix Infinipaint that would crash when started.

--- src/DisplayApp/DisplayApp.cpp
+++ src/DisplayApp/DisplayApp.cpp
@@ -17,6 +17,7 @@
 #include <DisplayApp/Screens/FirmwareUpdate.h>
 #include <DisplayApp/Screens/ApplicationList.h>
 #include <DisplayApp/Screens/FirmwareValidation.h>
+#include <DisplayApp/Screens/InfiniPaint.h>
 #include "../SystemTask/SystemTask.h"
 
 using namespace Pinetime::Applications;
@@ -200,6 +201,7 @@ void DisplayApp::RunningState() {
       case Apps::SysInfo: currentScreen.reset(new Screens::SystemInfo(this, dateTimeController, batteryController, brightnessController, bleController, watchdog)); break;
       case Apps::Meter: currentScreen.reset(new Screens::Meter(this)); break;
       case Apps::Gauge: currentScreen.reset(new Screens::Gauge(this)); break;
+      case Apps::Paint: currentScreen.reset(new Screens::InfiniPaint(this, lvgl)); break;
       case Apps::Brightness : currentScreen.reset(new Screens::Brightness(this, brightnessController)); break;
       case Apps::Music : currentScreen.reset(new Screens::Music(this, systemTask.nimble().music())); break;
       case Apps::FirmwareValidation: currentScreen.reset(new Screens::FirmwareValidation(this, validator)); break;
