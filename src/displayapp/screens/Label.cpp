13da1e38f019ef8989df1423164bd080768a7e04
Avamander <avamander@gmail.com>
2020-10-04 14:53:11 +0300

    Switched from NULL to nullptr

--- src/displayapp/screens/Label.cpp
+++ src/displayapp/screens/Label.cpp
@@ -4,7 +4,7 @@
 using namespace Pinetime::Applications::Screens;
 
 Label::Label(Pinetime::Applications::DisplayApp *app, const char *text)  : Screen(app), text{text} {
-  label = lv_label_create(lv_scr_act(), NULL);
+  label = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_align(label, LV_LABEL_ALIGN_LEFT);
   lv_obj_set_size(label, 240, 240);
   lv_label_set_text(label, text);
