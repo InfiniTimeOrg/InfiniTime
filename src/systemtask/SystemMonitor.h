e85d1ffc625c73dcbb30c783707bfb6110af6a41
Avamander <avamander@gmail.com>
2020-10-04 15:09:17 +0300

    Replaced NULL with nullptr

--- src/systemtask/SystemMonitor.h
+++ src/systemtask/SystemMonitor.h
@@ -27,7 +27,7 @@ namespace Pinetime {
         void Process() const {
           if(xTaskGetTickCount() - lastTick > 10000) {
             NRF_LOG_INFO("---------------------------------------\nFree heap : %d", xPortGetFreeHeapSize());
-            auto nb = uxTaskGetSystemState(tasksStatus, 10, NULL);
+            auto nb = uxTaskGetSystemState(tasksStatus, 10, nullptr);
             for (uint32_t i = 0; i < nb; i++) {
               NRF_LOG_INFO("Task [%s] - %d", tasksStatus[i].pcTaskName, tasksStatus[i].usStackHighWaterMark);
               if (tasksStatus[i].usStackHighWaterMark < 20)
