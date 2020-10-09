fd110dabe82f100550a7538c0e3eb3a439c9a7d2
Avamander <avamander@gmail.com>
2020-10-09 11:35:32 +0300

    Reformatted InfiniPaint again

--- src/displayapp/screens/InfiniPaint.cpp
+++ src/displayapp/screens/InfiniPaint.cpp
@@ -7,7 +7,7 @@ using namespace Pinetime::Applications::Screens;
 extern lv_font_t jetbrains_mono_extrabold_compressed;
 extern lv_font_t jetbrains_mono_bold_20;
 
-InfiniPaint::InfiniPaint(Pinetime::Applications::DisplayApp *app, Pinetime::Components::LittleVgl &lvgl) : Screen(app), lvgl{lvgl} {
+InfiniPaint::InfiniPaint(Pinetime::Applications::DisplayApp* app, Pinetime::Components::LittleVgl& lvgl) : Screen(app), lvgl{lvgl} {
   app->SetTouchMode(DisplayApp::TouchModes::Polling);
   std::fill(b, b + bufferSize, LV_COLOR_WHITE);
 }
