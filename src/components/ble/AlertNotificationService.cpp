920b2ef2997ac25c506a9325edb195a941498469
Robin Karlsson <robin.karlsson@protonmail.com>
2020-10-27 21:40:09 +0100

    remove switch statement

--- src/components/ble/AlertNotificationService.cpp
+++ src/components/ble/AlertNotificationService.cpp
@@ -60,48 +60,12 @@ int AlertNotificationService::OnAlert(uint16_t conn_handle, uint16_t attr_handle
     uint8_t *data = (uint8_t*) malloc(notifSize + 1);
     data[notifSize] = '\0';
     os_mbuf_copydata(ctxt->om, 0, notifSize, data);
-    char alertCategory = (char) data[0];
     char *s = (char *) &data[1];
     NRF_LOG_INFO("DATA : %s", s);
 
-    Pinetime::Controllers::NotificationManager::Categories messageCategory;
-    switch(alertCategory) {
-      case ALERT_UNKNOWN:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::Unknown;
-        break;
-      case ALERT_SIMPLE_ALERT:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::SimpleAlert;
-        break;
-      case ALERT_EMAIL:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::Email;
-        break;
-      case ALERT_NEWS:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::News;
-        break;
-      case ALERT_INCOMING_CALL:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::IncomingCall;
-        break;
-      case ALERT_MISSED_CALL:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::MissedCall;
-        break;
-      case ALERT_SMS:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::Sms;
-        break;
-      case ALERT_VOICE_MAIL:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::VoiceMail;
-        break;
-      case ALERT_SCHEDULE:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::Schedule;
-        break;
-      case ALERT_HIGH_PRIORITY_ALERT:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::HighProriotyAlert;
-        break;
-      case ALERT_INSTANT_MESSAGE:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::InstantMessage;
-        break;
-      default:
-        messageCategory = Pinetime::Controllers::NotificationManager::Categories::Unknown;
-    }
+    uint32_t category = data[0];
+    Pinetime::Controllers::NotificationManager::Categories messageCategory = static_cast<Pinetime::Controllers::NotificationManager::Categories>(category);
+
     m_notificationManager.Push(messageCategory, s, notifSize);
     m_systemTask.PushMessage(Pinetime::System::SystemTask::Messages::OnNewNotification);
     free(data);
