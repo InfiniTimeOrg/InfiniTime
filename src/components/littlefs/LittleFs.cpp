c096b2ae08f55e17628626dfd78d8514e4b398e4
redengin <sholstei@yahoo.com>
2021-01-11 01:12:11 -0500

    code complete

---
+++ src/components/littlefs/LittleFs.cpp
@@ -0,0 +1,100 @@
+#include "LittleFs.h"
+
+using namespace Pinetime::System;
+
+static int read(const struct lfs_config *c, lfs_block_t block,
+        lfs_off_t off, void *buffer, lfs_size_t size);
+
+static int prog(const struct lfs_config *c, lfs_block_t block,
+        lfs_off_t off, const void *buffer, lfs_size_t size);
+
+static int erase(const struct lfs_config *c, lfs_block_t block);
+
+static int sync(const struct lfs_config *c);
+
+constexpr size_t BLOCK_SIZE_BYTES = 4096u;
+constexpr size_t PAGE_SIZE_BYTES = 256u;
+constexpr size_t CACHE_PAGE_COUNT = 1u;
+constexpr size_t CACHE_SIZE_BYTES = (CACHE_PAGE_COUNT * PAGE_SIZE_BYTES);
+constexpr size_t LOOKAHEAD_SIZE_BYTES = (CACHE_PAGE_COUNT * 8u);
+static uint8_t readBuffer[CACHE_SIZE_BYTES];
+static uint8_t progBuffer[CACHE_SIZE_BYTES];
+static uint8_t __attribute__((aligned(32)))
+    lookaheadBuffer[LOOKAHEAD_SIZE_BYTES];
+const static struct lfs_config baseLfsConfig = {
+    .read = read,
+    .prog = prog,
+    .erase = erase,
+    .sync = sync,
+
+    .read_size = PAGE_SIZE_BYTES,
+    .prog_size = PAGE_SIZE_BYTES,
+    .block_size = BLOCK_SIZE_BYTES,
+    .block_cycles = 500u,
+
+    .cache_size = CACHE_SIZE_BYTES,
+    .lookahead_size = LOOKAHEAD_SIZE_BYTES,
+
+    .read_buffer = readBuffer,
+    .prog_buffer = progBuffer,
+    .lookahead_buffer = lookaheadBuffer,
+
+    .name_max = 0u, /** use LFS default */
+    .file_max = 0u, /** use LFS default */
+    .attr_max = 0u, /** use LFS default */
+};
+
+constexpr struct lfs_config createLfsConfig(Pinetime::System::LittleFs& lfs, const size_t totalSize_bytes) {
+    struct lfs_config config = baseLfsConfig;
+    config.context = &lfs;
+    config.block_count = totalSize_bytes / BLOCK_SIZE_BYTES;
+    return config;
+}
+
+LittleFs::LittleFs(Pinetime::Drivers::SpiNorFlash& driver,
+         const size_t startAddress,
+         const size_t size_bytes,
+         const bool allowFormat)
+:mDriver(driver), mStartAddress(startAddress), mSize_bytes(size_bytes),
+ mLfsConfig(createLfsConfig(*this, size_bytes))
+{
+    // try mount
+    if(0u != lfs_mount(&mLfs, &mLfsConfig)) {
+        if(allowFormat) {
+            // format and mount
+            if(0u == lfs_format(&mLfs, &mLfsConfig)) {
+                lfs_mount(&mLfs, &mLfsConfig);
+            }
+        }
+    }
+}
+
+static int read(const struct lfs_config *c, lfs_block_t block,
+                lfs_off_t off, void *buffer, lfs_size_t size) {
+    Pinetime::System::LittleFs& lfs = *(reinterpret_cast<Pinetime::System::LittleFs*>(c->context));
+    const size_t address = lfs.mStartAddress + (block * BLOCK_SIZE_BYTES) + off;
+    lfs.mDriver.Read(address, (uint8_t*)buffer, size);
+    // TODO assumes READ was successful
+    return 0u;
+}
+
+static int prog(const struct lfs_config *c, lfs_block_t block,
+        lfs_off_t off, const void *buffer, lfs_size_t size) {
+    Pinetime::System::LittleFs& lfs = *(reinterpret_cast<Pinetime::System::LittleFs*>(c->context));
+    const size_t address = lfs.mStartAddress + (block * BLOCK_SIZE_BYTES) + off;
+    lfs.mDriver.Write(address, (uint8_t*)buffer, size);
+    return lfs.mDriver.ProgramFailed() ? -1u : 0u;
+}
+
+static int erase(const struct lfs_config *c, lfs_block_t block) {
+    Pinetime::System::LittleFs& lfs = *(reinterpret_cast<Pinetime::System::LittleFs*>(c->context));
+    const size_t address = lfs.mStartAddress + (block * BLOCK_SIZE_BYTES);
+    lfs.mDriver.SectorErase(address);
+    return lfs.mDriver.EraseFailed() ? -1u : 0u;
+}
+
+static int sync(const struct lfs_config *c) {
+    // no hardware caching used
+    return 0u;
+}
+
---
+++ src/components/littlefs/LittleFs.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <SpiNorFlash.h>
+#include <lfs.h>
+
+namespace Pinetime {
+    namespace System {
+        class LittleFs {
+            public:
+                class LittleFsFile;
+                class LittleFsDir;
+                explicit LittleFs(Pinetime::Drivers::SpiNorFlash&,
+                                  const size_t startAddress,    ///< address of first block to use
+                                  const size_t size_bytes,      ///< must be a multiple of block size
+                                  const bool allowFormat = true ///< if filesystem not detected, format the space
+                                  );
+                LittleFs(const LittleFs&) = delete;
+                LittleFs& operator=(const LittleFs&) = delete;
+                LittleFs(LittleFs&&) = delete;
+                LittleFs& operator=(LittleFs&&) = delete;
+
+                bool remove(const char* path) { return 0u == lfs_remove(&mLfs, path); }
+                bool rename(const char* oldpath, const char* newpath) { return 0u == lfs_rename(&mLfs, oldpath, newpath); }
+                ssize_t filesize(const char* path) {
+                    struct lfs_info info;
+                    if(0u == lfs_stat(&mLfs, path, &info))
+                        return info.size;
+                    return -1;
+                }
+                /// file is closed upon destructor of the object
+                LittleFsFile& open(const char* const path,
+                                  const int flags           ///< @see lfs_open_flags
+                                  ) { return *(new LittleFsFile(mLfs, path, flags)); }
+                bool mkdir(const char* path) { return 0u == lfs_mkdir(&mLfs, path); }
+                LittleFsDir& opendir(const char* path) { return *(new LittleFsDir(mLfs, path)); }
+
+                const size_t mStartAddress;
+                const size_t mSize_bytes;
+                Pinetime::Drivers::SpiNorFlash& mDriver;
+
+            private:
+                lfs_t mLfs;
+                const struct lfs_config mLfsConfig;
+
+            public:
+                class LittleFsFile {
+                    public:
+                        explicit LittleFsFile(lfs_t& lfs, const char* const path, const int flags)
+                            : mLfs(lfs) { lfs_file_open(&mLfs, &mLfs_file, path, flags); }
+                        LittleFsFile(const LittleFsFile&) = delete;
+                        LittleFsFile& operator=(const LittleFsFile&) = delete;
+                        LittleFsFile(LittleFsFile&&) = delete;
+                        LittleFsFile& operator=(LittleFsFile&&) = delete;
+                        ~LittleFsFile() {
+                            lfs_file_close(&mLfs, &mLfs_file);
+                        }
+                        ssize_t read(void* buffer, size_t size) { return lfs_file_read(&mLfs, &mLfs_file, buffer, size); }
+                        ssize_t write(const void* buffer, size_t size) { return lfs_file_write(&mLfs, &mLfs_file, buffer, size); }
+                        ssize_t seek(size_t offset, size_t whence) { return 0u == lfs_file_seek(&mLfs, &mLfs_file, offset, whence); }
+                        bool truncate(size_t sz) { return 0u == lfs_file_truncate(&mLfs, &mLfs_file, sz); }
+                        /// return the current position within the file
+                        ssize_t tell() { return lfs_file_tell(&mLfs, &mLfs_file); }
+                        ssize_t size() { return lfs_file_size(&mLfs, &mLfs_file); }
+                    private:
+                        lfs_t& mLfs;
+                        lfs_file_t mLfs_file;
+                };
+
+                class LittleFsDir {
+                    public:
+                        explicit LittleFsDir(lfs_t& lfs, const char* path)
+                            : mLfs(lfs) { lfs_dir_open(&mLfs, &mLfs_dir, path); }
+                        LittleFsDir(const LittleFsDir&) = delete;
+                        LittleFsDir& operator=(const LittleFsDir&) = delete;
+                        LittleFsDir(LittleFsDir&&) = delete;
+                        LittleFsDir& operator=(LittleFsDir&&) = delete;
+                        ~LittleFsDir() {
+                            lfs_dir_close(&mLfs, &mLfs_dir);
+                        }
+                        // TODO determine API for directories
+                    private:
+                        lfs_t& mLfs;
+                        lfs_dir_t mLfs_dir;
+                };
+        };
+    }
+}
