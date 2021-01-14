b8f943e73c84e368ebe6a380a7ca231e2a81f428
panky-codes <pankaj.sarathy1992@gmail.com>
2021-01-14 21:22:36 +0100

    Implemented a circular buffer and added an average alg for batter remaining percentage.

--- src/components/battery/BatteryController.h
+++ src/components/battery/BatteryController.h
@@ -1,14 +1,41 @@
 #pragma once
 #include <cstdint>
 #include <drivers/include/nrfx_saadc.h>
+#include <array>
+#include <numeric>
 
 namespace Pinetime {
   namespace Controllers {
+  // A simple circular buffer that can be used to average 
+  // out sensor values
+  template <int N> 
+  class CircBuffer {
+  public:
+    CircBuffer() : arr{}, sz{}, cap{N}, loc{} {}
+    void insert(const float num) {
+      loc %= cap;
+      arr[loc++] = num;
+      if (sz != cap) {
+        sz++;
+      }
+    }
+
+    float GetAverage() const {
+      float sum = std::accumulate(arr.begin(), arr.end(), 0);
+      return (sum / sz);
+    }
+
+  private:
+    std::array<float, N> arr;
+    uint8_t sz;
+    uint8_t cap;
+    uint8_t loc;
+  };
     class Battery {
       public:
         void Init();
         void Update();
-        float PercentRemaining() const { return percentRemaining; }
+        float PercentRemaining() const { return percentRemainingBuffer.GetAverage(); }
         float Voltage() const { return voltage; }
         bool IsCharging() const { return isCharging; }
         bool IsPowerPresent() const { return isPowerPresent; }
@@ -17,8 +44,9 @@ namespace Pinetime {
         static constexpr uint32_t chargingPin = 12;
         static constexpr uint32_t powerPresentPin = 19;
         static constexpr nrf_saadc_input_t batteryVoltageAdcInput = NRF_SAADC_INPUT_AIN7;
+        static constexpr uint8_t percentRemainingSamples = 10;
         static void SaadcEventHandler(nrfx_saadc_evt_t const * p_event);
-        float percentRemaining = 0.0f;
+        CircBuffer<percentRemainingSamples> percentRemainingBuffer {};
         float voltage = 0.0f;
         bool isCharging = false;
         bool isPowerPresent = false;
