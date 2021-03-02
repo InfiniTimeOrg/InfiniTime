81e0d92c6aa8900ee1450ae5e669d6a3c6d930d3
CK Kung <kung1388@hotmail.com>
2021-03-02 21:47:16 +0800

    Update BatteryController.h

--- src/components/battery/BatteryController.h
+++ src/components/battery/BatteryController.h
@@ -48,7 +48,7 @@ namespace Pinetime {
         bool IsPowerPresent() const { return isPowerPresent; }
 
       private:
-        static constexpr uint32_t chargingPin = 12;
+        static constexpr uint32_t chargingPin = 19;
         static constexpr uint32_t powerPresentPin = 19;
         static constexpr nrf_saadc_input_t batteryVoltageAdcInput = NRF_SAADC_INPUT_AIN7;
         static constexpr uint8_t percentRemainingSamples = 10;
