8fd5d6640d49dd11f25fa64a031e6aed327cd84e
James A. Jerkins <evergreen@jamesjerkinscomputer.com>
2021-07-30 21:42:57 -0500

    Set notification vibration proportional to display timeout

--- src/systemtask/SystemTask.cpp
+++ src/systemtask/SystemTask.cpp
@@ -266,7 +266,7 @@ void SystemTask::Work() {
           if (isSleeping && !isWakingUp) {
             GoToRunning();
           }
-          motorController.SetDuration(35);
+          motorController.SetDuration(settingsController.GetScreenTimeOut()/142);
           displayApp.PushMessage(Pinetime::Applications::Display::Messages::NewNotification);
           break;
         case Messages::OnTimerDone:
