085d27bf6c6b301bbb0421a98d20618ac33e2afd
aemkai <87898467+aemkai@users.noreply.github.com>
2021-08-02 18:16:28 +0200

    Update WatchFaceBinary.h

--- src/displayapp/screens/WatchFaceBinary.h
+++ src/displayapp/screens/WatchFaceBinary.h
@@ -72,50 +72,50 @@ namespace Pinetime {
         lv_obj_t* stepValue;
         lv_obj_t* notificationIcon;
 
-		// Binary Watch
-		// Time
-		#define LED_SIZE1	37
-		#define LED_RING1	2
-		#define LED_SPACE_H1   (LED_SIZE1/12)
-		#define LED_SPACE_V1   (LED_SIZE1/4)
-		#define LED_COL_MIN_ON		lv_color_hex(0xDD0000)
-		#define LED_COL_MIN_OFF		lv_color_hex(0x220000)
-		#define LED_COL_HOUR_ON		lv_color_hex(0x00DD00)
-		#define LED_COL_HOUR_OFF	lv_color_hex(0x002200)
-		#define LED_COL_RING		lv_color_hex(0x999999)
-		lv_obj_t * minLED0;
-		lv_obj_t * minLED1;
-		lv_obj_t * minLED2;
-		lv_obj_t * minLED3;
-		lv_obj_t * minLED4;
-		lv_obj_t * minLED5;
+	// Binary Watch
+	// Time
+	#define LED_SIZE1	37
+	#define LED_RING1	2
+	#define LED_SPACE_H1   (LED_SIZE1/12)
+	#define LED_SPACE_V1   (LED_SIZE1/4)
+	#define LED_COL_MIN_ON		lv_color_hex(0xDD0000)
+	#define LED_COL_MIN_OFF		lv_color_hex(0x220000)
+	#define LED_COL_HOUR_ON		lv_color_hex(0x00DD00)
+	#define LED_COL_HOUR_OFF	lv_color_hex(0x002200)
+	#define LED_COL_RING		lv_color_hex(0x999999)
+	lv_obj_t * minLED0;
+	lv_obj_t * minLED1;
+	lv_obj_t * minLED2;
+	lv_obj_t * minLED3;
+	lv_obj_t * minLED4;
+	lv_obj_t * minLED5;
 
-		lv_obj_t * hourLED0;
-		lv_obj_t * hourLED1;
-		lv_obj_t * hourLED2;
-		lv_obj_t * hourLED3;
-		lv_obj_t * hourLED4;
-		
-		// Date
-		#define LED_SIZE2	18
-		#define LED_RING2	2
-		#define LED_SPACE_H2   (LED_SIZE2/6)
-		#define LED_SPACE_V2   (LED_SIZE2/4)
-		#define LED_COL_DAY_ON		lv_color_hex(0xCCCCCC)
-		#define LED_COL_DAY_OFF		lv_color_hex(0x222222)	
-		#define LED_COL_MON_ON		lv_color_hex(0xCCCCCC)
-		#define LED_COL_MON_OFF		lv_color_hex(0x222222)	
-		#define LED_COL_RING		lv_color_hex(0x999999)		
-		lv_obj_t * dayLED0;
-		lv_obj_t * dayLED1;
-		lv_obj_t * dayLED2;
-		lv_obj_t * dayLED3;
-		lv_obj_t * dayLED4;
+	lv_obj_t * hourLED0;
+	lv_obj_t * hourLED1;
+	lv_obj_t * hourLED2;
+	lv_obj_t * hourLED3;
+	lv_obj_t * hourLED4;
 
-		lv_obj_t * monLED0;
-		lv_obj_t * monLED1;
-		lv_obj_t * monLED2;
-		lv_obj_t * monLED3;
+	// Date
+	#define LED_SIZE2	18
+	#define LED_RING2	2
+	#define LED_SPACE_H2   (LED_SIZE2/6)
+	#define LED_SPACE_V2   (LED_SIZE2/4)
+	#define LED_COL_DAY_ON		lv_color_hex(0xCCCCCC)
+	#define LED_COL_DAY_OFF		lv_color_hex(0x222222)	
+	#define LED_COL_MON_ON		lv_color_hex(0xCCCCCC)
+	#define LED_COL_MON_OFF		lv_color_hex(0x222222)	
+	#define LED_COL_RING		lv_color_hex(0x999999)		
+	lv_obj_t * dayLED0;
+	lv_obj_t * dayLED1;
+	lv_obj_t * dayLED2;
+	lv_obj_t * dayLED3;
+	lv_obj_t * dayLED4;
+
+	lv_obj_t * monLED0;
+	lv_obj_t * monLED1;
+	lv_obj_t * monLED2;
+	lv_obj_t * monLED3;
 
 
 
