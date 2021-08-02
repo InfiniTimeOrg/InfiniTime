711920b0d4b65b81a097f789309b778f4ff66d6b
aemkai <87898467+aemkai@users.noreply.github.com>
2021-08-02 18:14:19 +0200

    Update Clock.h

--- src/displayapp/screens/Clock.h
+++ src/displayapp/screens/Clock.h
@@ -51,6 +51,7 @@ namespace Pinetime {
         std::unique_ptr<Screen> WatchFaceDigitalScreen();
         std::unique_ptr<Screen> WatchFaceAnalogScreen();
         std::unique_ptr<Screen> PineTimeStyleScreen();
+        std::unique_ptr<Screen> WatchFaceBinaryScreen();
 
         // Examples for more watch faces
         // std::unique_ptr<Screen> WatchFaceMinimalScreen();
