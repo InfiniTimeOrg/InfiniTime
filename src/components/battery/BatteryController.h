e0b5bd72d4b7b910eb09b7aaaca04872f7f5e8b4
hassless <85612141+hassless@users.noreply.github.com>
2021-08-15 12:25:15 +0200

    Update BatteryController.h

--- src/components/battery/BatteryController.h
+++ src/components/battery/BatteryController.h
@@ -49,6 +49,7 @@ namespace Pinetime {
 
       void SaadcEventHandler(nrfx_saadc_evt_t const* p_event);
       static void AdcCallbackStatic(nrfx_saadc_evt_t const* event);
+      static int GetBatteryPercentageFromVoltage(float voltage);
 
       bool isReading = false;
 
