2b30ff4fc6a1835bdc5a94f8754cfd366885c4c0
Riku Isokoski <riksu9000@gmail.com>
2021-08-19 11:12:34 +0300

    Remove unused variables

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -136,9 +136,6 @@ void DisplayApp::InitHw() {
   brightnessController.Set(settingsController.GetBrightness());
 }
 
-uint32_t acc = 0;
-uint32_t count = 0;
-bool toggle = true;
 void DisplayApp::Refresh() {
   TickType_t queueTimeout;
   TickType_t delta;
