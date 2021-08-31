acdad244560d9348068e68f91a4dc45aeea9974a
Antoine Dehut <a.dehut@nextkidney.com>
2021-08-31 10:19:03 +0200

    fix build problems

--- src/displayapp/screens/ScoreApp.cpp
+++ src/displayapp/screens/ScoreApp.cpp
@@ -11,17 +11,18 @@ static void btnEventHandler(lv_obj_t* obj, lv_event_t event) {
   screen->OnButtonEvent(obj, event);
 }
 
-lv_obj_t* ScoreApp::_createButton(lv_align_t alignment, uint8_t x, uint8_t y, uint8_t width, uint8_t height, char text[]){
-  lv_obj_t *button;
+ScoreApp::widget_t ScoreApp::_createButton(lv_align_t alignment, uint8_t x, uint8_t y, uint8_t width, uint8_t height, const char text[]){
 
-  button = lv_btn_create(lv_scr_act(), nullptr);
-  button->user_data = this;
-  lv_obj_set_event_cb(button, btnEventHandler);
-  lv_obj_align(button, lv_scr_act(), alignment, x, y);
-  lv_obj_set_width(button, width);
-  lv_obj_set_height(button, height);
-  txtMUp = lv_label_create(button, nullptr);
-  lv_label_set_text(txtMUp, text);
+  widget_t button;
+
+  button.button = lv_btn_create(lv_scr_act(), nullptr);
+  button.button->user_data = this;
+  lv_obj_set_event_cb(button.button, btnEventHandler);
+  lv_obj_align(button.button, lv_scr_act(), alignment, x, y);
+  lv_obj_set_width(button.button, width);
+  lv_obj_set_height(button.button, height);
+  button.label = lv_label_create(button.button, nullptr);
+  lv_label_set_text(button.label, text);
 
   return button;
 }
@@ -30,13 +31,13 @@ ScoreApp::ScoreApp(DisplayApp* app) : Screen(app) {
     
   btnMyScore = _createButton(LV_ALIGN_IN_TOP_LEFT, 0, 0, scoreWidth, scoreHeight, "0");
   btnYourScore = _createButton(LV_ALIGN_IN_BOTTOM_LEFT, 0, 0, scoreWidth, scoreHeight, "0");
-  lv_label_set_text_fmt(btnMyScore, "%d", myScore);
-  lv_label_set_text(btnYourScore, "%d", yourScore);
+  lv_label_set_text_fmt(btnMyScore.label, "%d", myScore);
+  lv_label_set_text_fmt(btnYourScore.label, "%d", yourScore);
   
   btnMyScoreMinus = _createButton(LV_ALIGN_IN_TOP_RIGHT, 0, 0, minusWidth, minusHeight, "-1");
   btnYourScoreMinus = _createButton(LV_ALIGN_IN_BOTTOM_RIGHT, 0, 0, minusWidth, minusHeight, "-1");
   
-  btnReset = _createButton(LV_ALIGN_IN_MID_RIGHT, 0, 0, resetWidth, resetHeight, "Rst");
+  btnReset = _createButton(LV_ALIGN_CENTER, 20, 0, resetWidth, resetHeight, "Rst");
 
   taskRefresh = lv_task_create(RefreshTaskCallback, LV_DISP_DEF_REFR_PERIOD, LV_TASK_PRIO_MID, this);
 }
@@ -47,29 +48,26 @@ ScoreApp::~ScoreApp() {
 }
 
 void ScoreApp::Refresh() {
-  lv_label_set_text_fmt(btnMyScore, "%d", myScore);
-  lv_label_set_text(btnYourScore, "%d", yourScore);
+  lv_label_set_text_fmt(btnMyScore.label, "%d", myScore);
+  lv_label_set_text_fmt(btnYourScore.label, "%d", yourScore);
 }
 
 void ScoreApp::OnButtonEvent(lv_obj_t* obj, lv_event_t event) {
   if (event == LV_EVENT_CLICKED) {
-    switch() {
-      case btnMyScore:
+    if (obj == btnMyScore.button)
         myScore++;
-        break;
-      case btnYourScore:
-        yourScore++;
-        break;
-      case btnMyScoreMinus:
-        myScore--;
-        break;
-      case btnYourScoreMinus:
-        yourScore--;
-        break;
-      case btnReset:
-        myScore = 0;
+    else if (obj == btnYourScore.button) {
+      yourScore++;
+    }
+    else if (obj == btnMyScoreMinus.button) {
+      myScore--;
+    }
+    else if (obj == btnYourScoreMinus.button) {
+      yourScore--;
+    }
+    else if (obj == btnReset.button) {
+      myScore = 0;
         yourScore = 0;
-        break;
     }
   }
 }
--- src/displayapp/screens/ScoreApp.h
+++ src/displayapp/screens/ScoreApp.h
@@ -1,42 +1,50 @@
 #pragma once
 
 #include "Screen.h"
-// #include "systemtask/SystemTask.h"
+#include "systemtask/SystemTask.h"
 #include "../LittleVgl.h"
+#include <lvgl/src/lv_core/lv_obj.h>
 
 namespace Pinetime::Applications::Screens {
 
   class ScoreApp : public Screen {
   public:
-  // TODO: Modes (tennis, other sports, magic the gathering/generic card game...)
+    // TODO: Modes (tennis, other sports, magic the gathering/generic card game...)
     // enum class Modes { Normal, Done };
 
     ScoreApp(DisplayApp* app);
 
     ~ScoreApp() override;
 
+    void Refresh() override;
+
     void OnButtonEvent(lv_obj_t* obj, lv_event_t event);
 
   private:
-      static constexpr uint8_t displayWidth = 240;
-      static constexpr uint8_t displayHeight = 240;
+    static constexpr uint8_t displayWidth = 240;
+    static constexpr uint8_t displayHeight = 240;
 
-      static constexpr uint8_t scoreWidth = (displayWidth * 60) / 100;
-      static constexpr uint8_t scoreHeight = displayHeight / 4;
+    static constexpr uint8_t scoreWidth = (displayWidth * 60) / 100;
+    static constexpr uint8_t scoreHeight = displayHeight / 4;
 
-      static constexpr uint8_t minusWidth = displayWidth - scoreWidth;
-      static constexpr uint8_t minusHeight = displayHeight / 4;
+    static constexpr uint8_t minusWidth = displayWidth - scoreWidth;
+    static constexpr uint8_t minusHeight = displayHeight / 4;
 
-      static constexpr uint8_t  resetWidth = displayWidth - scoreWidth;
-      static constexpr uint8_t  resetHeight = displayHeight / 4;
+    static constexpr uint8_t  resetWidth = displayWidth - scoreWidth;
+    static constexpr uint8_t  resetHeight = displayHeight / 4;
+
+    struct widget_t {
+      lv_obj_t* button;
+      lv_obj_t* label;
+    };
 
     uint8_t myScore = 0;
     uint8_t yourScore = 0;
 
-    lv_obj_t* _createButton(lv_align_t alignment, uint8_t x, uint8_t y, uint8_t width, uint8_t height, char text[])
+    widget_t _createButton(lv_align_t alignment, uint8_t x, uint8_t y, uint8_t width, uint8_t height, const char text[]);
 
     // TODO: Mode button not printed and not used
-    lv_obj_t *btnMyScore, *btnYourScore, *btnMyScoreMinus, *btnYourScoreMinus, *btnMode, *btnReset;
+    widget_t btnMyScore, btnYourScore, btnMyScoreMinus, btnYourScoreMinus, btnMode, btnReset;
 
     lv_task_t* taskRefresh;
   };
