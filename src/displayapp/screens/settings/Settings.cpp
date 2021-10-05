92b1e83e3efab14140fa48267dbb49c4210ea85c
Tim Keller <geekboy1011@gmail.com>
2021-10-05 03:29:49 +0000

    Remove "fancy" settings display and always show ShakeWakeThresholdSetting

--- src/displayapp/screens/settings/Settings.cpp
+++ src/displayapp/screens/settings/Settings.cpp
@@ -47,26 +47,22 @@ std::unique_ptr<Screen> Settings::CreateScreen1() {
 
 std::unique_ptr<Screen> Settings::CreateScreen2() {
 
-  std::array<Screens::List::Applications, 4> applications {{
-    {Symbols::shoe, "Steps", Apps::SettingSteps},
-    {Symbols::clock, "Set date", Apps::SettingSetDate},
-    {Symbols::clock, "Set time", Apps::SettingSetTime},
-    {Symbols::batteryHalf, "Battery", Apps::BatteryInfo}
-  }};
+  std::array<Screens::List::Applications, 4> applications {{{Symbols::shoe, "Steps", Apps::SettingSteps},
+                                                            {Symbols::clock, "Set date", Apps::SettingSetDate},
+                                                            {Symbols::clock, "Set time", Apps::SettingSetTime},
+                                                            {Symbols::batteryHalf, "Battery", Apps::BatteryInfo}}};
 
   return std::make_unique<Screens::List>(1, 3, app, settingsController, applications);
 }
 
 std::unique_ptr<Screen> Settings::CreateScreen3() {
 
-  std::array<Screens::List::Applications, 4> applications {{
-    {Symbols::check, "Firmware", Apps::FirmwareValidation},
-    {Symbols::list, "About", Apps::SysInfo},
-    {Symbols::none, "None", Apps::None},
-    {Symbols::none, "None", Apps::None}
+  std::array<Screens::List::Applications, 4> applications {{{Symbols::paintbrush, "PTS Colors", Apps::SettingPineTimeStyle},
+                                                            {Symbols::none, "Wake Sense", Apps::SettingShakeThreshold},
+                                                            {Symbols::check, "Firmware", Apps::FirmwareValidation},
+                                                            {Symbols::list, "About", Apps::SysInfo}
+
   }};
-  if (settingsController.isWakeUpModeOn(Pinetime::Controllers::Settings::WakeUpMode::Shake)) {
-    applications[1] = {Symbols::list, "Wake Sense", Apps::SettingShakeThreshold};
-  }
+
   return std::make_unique<Screens::List>(2, 3, app, settingsController, applications);
 }
