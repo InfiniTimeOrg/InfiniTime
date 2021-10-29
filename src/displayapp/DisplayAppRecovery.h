456084499464e242faf36606edada10ab6056939
James A. Jerkins <evergreen@jamesjerkinscomputer.com>
2021-10-28 20:38:59 -0500

    Fix recovery firmware build

--- src/displayapp/DisplayAppRecovery.h
+++ src/displayapp/DisplayAppRecovery.h
@@ -10,7 +10,7 @@
 #include <date/date.h>
 #include <drivers/Watchdog.h>
 #include <components/motor/MotorController.h>
-#include <BootErrors.h>
+#include "BootErrors.h"
 #include "TouchEvents.h"
 #include "Apps.h"
 #include "Messages.h"
