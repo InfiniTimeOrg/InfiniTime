4f0563a6607239c130eaa663e446be6e7fb63d79
TotalChaos05 <70387628+TotalChaos05@users.noreply.github.com>
2021-12-31 19:20:13 -0600

    FUCK

--- src/displayapp/screens/Clock.cpp
+++ src/displayapp/screens/Clock.cpp
@@ -71,7 +71,7 @@ std::unique_ptr<Screen> Clock::WatchFaceDigitalScreen() {
 }
 
 std::unique_ptr<Screen> Clock::WatchFaceAnalogScreen() {
-  return std::make_unique<Screens::WatchFaceDigital>(app,
+  return std::make_unique<Screens::WatchFaceAnalog>(app,
                                                      dateTimeController,
                                                      batteryController,
                                                      bleController,
@@ -81,7 +81,7 @@ std::unique_ptr<Screen> Clock::WatchFaceAnalogScreen() {
                                                      motionController);
 }
 std::unique_ptr<Screen> Clock::PineTimeStyleScreen() {
-  return std::make_unique<Screens::WatchFaceDigital>(app,
+  return std::make_unique<Screens::PineTimeStyle>(app,
                                                      dateTimeController,
                                                      batteryController,
                                                      bleController,
