9131722ffc3befa481b00a9816dd34d15ddeb0de
Reinhold Gschweicher <pyro4hell@gmail.com>
2022-01-27 23:10:59 +0100

    SystemMonitor: implement FreeRtosMonitor only if trace facility is set
    Split SystemMonitor into h and cpp file and move the logging code of the
    `Process` function into the cpp file.
    
    Depending of the `configUSE_TRACE_FACILITY` define from
    `src/FreeRTOSConfig.h` create either a "FreeRtosMonitor" or a
    "DummyMonitor".
    
    Make the `Process()` function non-const, as the FreeRtosMonitor changes
    the member variable `lastTick`.
    
    In `SystemTask.h` we then only need to use `SystemMonitor`, without
    knowledge of the `configUSE_TRACE_FACILITY` define.

--- src/systemtask/SystemMonitor.h
+++ src/systemtask/SystemMonitor.h
@@ -1,44 +1,16 @@
 #pragma once
-#include <FreeRTOS.h>
+#include <FreeRTOS.h> // declares configUSE_TRACE_FACILITY
 #include <task.h>
-#include <nrf_log.h>
 
 namespace Pinetime {
   namespace System {
-    struct DummyMonitor {};
-    struct FreeRtosMonitor {};
-
-    template <class T> class SystemMonitor {
+    class SystemMonitor {
     public:
-      SystemMonitor() = delete;
-    };
-
-    template <> class SystemMonitor<DummyMonitor> {
-    public:
-      void Process() const {
-      }
-    };
-
-    template <> class SystemMonitor<FreeRtosMonitor> {
-    public:
-      void Process() const {
-        if (xTaskGetTickCount() - lastTick > 10000) {
-          NRF_LOG_INFO("---------------------------------------\nFree heap : %d", xPortGetFreeHeapSize());
-          auto nb = uxTaskGetSystemState(tasksStatus, 10, nullptr);
-          for (uint32_t i = 0; i < nb; i++) {
-            NRF_LOG_INFO("Task [%s] - %d", tasksStatus[i].pcTaskName, tasksStatus[i].usStackHighWaterMark);
-            if (tasksStatus[i].usStackHighWaterMark < 20)
-              NRF_LOG_INFO("WARNING!!! Task %s task is nearly full, only %dB available",
-                           tasksStatus[i].pcTaskName,
-                           tasksStatus[i].usStackHighWaterMark * 4);
-          }
-          lastTick = xTaskGetTickCount();
-        }
-      }
-
+      void Process();
+#if configUSE_TRACE_FACILITY == 1
     private:
       mutable TickType_t lastTick = 0;
-      mutable TaskStatus_t tasksStatus[10];
+#endif
     };
   }
 }
