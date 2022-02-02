4d850281bedf342d0856da5eafc22e46d0767c56
Марков Дмитрий <13werwolf13@mail.ru>
2022-02-02 09:11:17 +0500

    Update src/displayapp/screens/WatchFaceTerminal.cpp
    Co-authored-by: NeroBurner <pyro4hell@gmail.com>

--- src/displayapp/screens/WatchFaceTerminal.cpp
+++ src/displayapp/screens/WatchFaceTerminal.cpp
@@ -111,9 +111,13 @@ void WatchFaceTerminal::Refresh() {
   batteryPercentRemaining = batteryController.PercentRemaining();
   if (batteryPercentRemaining.IsUpdated()) {
     auto batteryPercent = batteryPercentRemaining.Get();
+    if (batteryPercent == 100) {
+      lv_obj_set_style_local_text_color(batteryIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_GREEN);
+    } else {
+      lv_obj_set_style_local_text_color(batteryIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    }
     lv_label_set_text(batteryIcon, BatteryIcon::GetBatteryIcon(batteryPercent));
-    auto isCharging = batteryController.IsCharging() || batteryController.IsPowerPresent();
-    lv_label_set_text(batteryPlug, BatteryIcon::GetPlugIcon(isCharging));
+    lv_label_set_text_fmt(batteryValue, "[BATT]#387b54 %d%\%#", batteryPercent);
   }
 
   bleState = bleController.IsConnected();
