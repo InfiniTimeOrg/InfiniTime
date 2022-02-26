a89fca7f86eead50ab1ace17827c13e6abe3a582
Diego Miguel <dmlls@diegomiguel.me>
2022-02-26 20:57:33 +0100

    Add 'circle' symbol & Pine64 logo

--- src/displayapp/screens/Symbols.h
+++ src/displayapp/screens/Symbols.h
@@ -37,6 +37,7 @@ namespace Pinetime {
         static constexpr const char* chartLine = "\xEF\x88\x81";
         static constexpr const char* eye = "\xEF\x81\xAE";
         static constexpr const char* home = "\xEF\x80\x95";
+        static constexpr const char* circle = "\xEF\x84\x91";
 
         // lv_font_sys_48.c
         static constexpr const char* settings = "\xEE\xA4\x82"; // e902
