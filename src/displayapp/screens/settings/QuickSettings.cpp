ac99997909c3d202ba894dcfb869fc98c6031f37
mjhershfield <64646677+mjhershfield@users.noreply.github.com>
2022-03-12 00:53:40 +0000

    Update QuickSettings to use ColorScheme colors

--- src/displayapp/screens/settings/QuickSettings.cpp
+++ src/displayapp/screens/settings/QuickSettings.cpp
@@ -38,10 +38,13 @@ QuickSettings::QuickSettings(Pinetime::Applications::DisplayApp* app,
   lv_label_set_text(label_time, dateTimeController.FormattedTime().c_str());
   lv_label_set_align(label_time, LV_LABEL_ALIGN_CENTER);
   lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_IN_TOP_LEFT, 0, 0);
+  lv_obj_set_style_local_text_color(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getOnBackgroundColor());
+
 
   batteryIcon = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text(batteryIcon, BatteryIcon::GetBatteryIcon(batteryController.PercentRemaining()));
   lv_obj_align(batteryIcon, nullptr, LV_ALIGN_IN_TOP_RIGHT, 0, 0);
+  lv_obj_set_style_local_text_color(batteryIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getOnBackgroundColor());
 
   static constexpr uint8_t barHeight = 20 + innerDistance;
   static constexpr uint8_t buttonHeight = (LV_VER_RES_MAX - barHeight - innerDistance) / 2;
@@ -51,7 +54,7 @@ QuickSettings::QuickSettings(Pinetime::Applications::DisplayApp* app,
 
   lv_style_init(&btn_style);
   lv_style_set_radius(&btn_style, LV_STATE_DEFAULT, buttonHeight / 4);
-  lv_style_set_bg_color(&btn_style, LV_STATE_DEFAULT, lv_color_hex(0x111111));
+  lv_style_set_bg_color(&btn_style, LV_STATE_DEFAULT, settingsController.getSurfaceColor());
 
   btn1 = lv_btn_create(lv_scr_act(), nullptr);
   btn1->user_data = this;
@@ -63,6 +66,7 @@ QuickSettings::QuickSettings(Pinetime::Applications::DisplayApp* app,
   btn1_lvl = lv_label_create(btn1, nullptr);
   lv_obj_set_style_local_text_font(btn1_lvl, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_48);
   lv_label_set_text_static(btn1_lvl, brightness.GetIcon());
+  lv_obj_set_style_local_text_color(btn1_lvl, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getOnSurfaceColor());
 
   btn2 = lv_btn_create(lv_scr_act(), nullptr);
   btn2->user_data = this;
@@ -75,18 +79,20 @@ QuickSettings::QuickSettings(Pinetime::Applications::DisplayApp* app,
   lbl_btn = lv_label_create(btn2, nullptr);
   lv_obj_set_style_local_text_font(lbl_btn, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_48);
   lv_label_set_text_static(lbl_btn, Symbols::highlight);
+  lv_obj_set_style_local_text_color(lbl_btn, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getOnSurfaceColor());
 
   btn3 = lv_btn_create(lv_scr_act(), nullptr);
   btn3->user_data = this;
   lv_obj_set_event_cb(btn3, ButtonEventHandler);
   lv_btn_set_checkable(btn3, true);
   lv_obj_add_style(btn3, LV_BTN_PART_MAIN, &btn_style);
-  lv_obj_set_style_local_bg_color(btn3, LV_BTN_PART_MAIN, LV_STATE_CHECKED, LV_COLOR_GREEN);
+  lv_obj_set_style_local_bg_color(btn3, LV_BTN_PART_MAIN, LV_STATE_CHECKED, settingsController.getPrimaryColor());
   lv_obj_set_size(btn3, buttonWidth, buttonHeight);
   lv_obj_align(btn3, nullptr, LV_ALIGN_IN_BOTTOM_LEFT, buttonXOffset, 0);
 
   btn3_lvl = lv_label_create(btn3, nullptr);
   lv_obj_set_style_local_text_font(btn3_lvl, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_48);
+  lv_obj_set_style_local_text_color(btn3_lvl, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getOnSurfaceColor());
 
   if (settingsController.GetNotificationStatus() == Controllers::Settings::Notification::ON) {
     lv_obj_add_state(btn3, LV_STATE_CHECKED);
@@ -105,12 +111,15 @@ QuickSettings::QuickSettings(Pinetime::Applications::DisplayApp* app,
   lbl_btn = lv_label_create(btn4, nullptr);
   lv_obj_set_style_local_text_font(lbl_btn, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_48);
   lv_label_set_text_static(lbl_btn, Symbols::settings);
+  lv_obj_set_style_local_text_color(lbl_btn, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getOnSurfaceColor());
 
   lv_obj_t* backgroundLabel = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_long_mode(backgroundLabel, LV_LABEL_LONG_CROP);
   lv_obj_set_size(backgroundLabel, 240, 240);
   lv_obj_set_pos(backgroundLabel, 0, 0);
   lv_label_set_text_static(backgroundLabel, "");
+  lv_obj_set_style_local_bg_color(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, settingsController.getBackgroundColor());
+  lv_obj_set_style_local_bg_opa(label_time, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_100);
 
   taskUpdate = lv_task_create(lv_update_task, 5000, LV_TASK_PRIO_MID, this);
 }
