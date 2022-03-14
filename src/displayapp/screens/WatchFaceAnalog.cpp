1379b7902fca8628614af358bac7d546cc94efdc
Reinhold Gschweicher <pyro4hell@gmail.com>
2022-03-14 20:33:14 +0100

    WatchFaceAnalog: local date/time variables
    Use local date and time variables. No need to store them in the object.

--- src/displayapp/screens/WatchFaceAnalog.cpp
+++ src/displayapp/screens/WatchFaceAnalog.cpp
@@ -137,9 +137,9 @@ WatchFaceAnalog::~WatchFaceAnalog() {
 }
 
 void WatchFaceAnalog::UpdateClock() {
-  hour = dateTimeController.Hours();
-  minute = dateTimeController.Minutes();
-  second = dateTimeController.Seconds();
+  uint8_t hour = dateTimeController.Hours();
+  uint8_t minute = dateTimeController.Minutes();
+  uint8_t second = dateTimeController.Seconds();
 
   if (sMinute != minute) {
     auto const angle = minute * 6;
@@ -214,9 +214,9 @@ void WatchFaceAnalog::Refresh() {
   currentDateTime = dateTimeController.CurrentDateTime();
 
   if (currentDateTime.IsUpdated()) {
-    month = dateTimeController.Month();
-    day = dateTimeController.Day();
-    dayOfWeek = dateTimeController.DayOfWeek();
+    Pinetime::Controllers::DateTime::Months month = dateTimeController.Month();
+    uint8_t day = dateTimeController.Day();
+    Pinetime::Controllers::DateTime::Days dayOfWeek = dateTimeController.DayOfWeek();
 
     UpdateClock();
 
