94173a2dd2bd1f1f35cdce639a58350f47fc12f7
Jean-François Milants <jf@codingfield.com>
2022-03-27 20:21:44 +0200

    Timer App : add background label to ensure that the app will be displayed correctly after a full refresh (HW scrolling transition). Code cleaning and rename methods.

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -207,7 +207,7 @@ void DisplayApp::Refresh() {
       case Messages::TimerDone:
         if (currentApp == Apps::Timer) {
           auto* timer = static_cast<Screens::Timer*>(currentScreen.get());
-          timer->setDone();
+          timer->SetDone();
         } else {
           LoadApp(Apps::Timer, DisplayApp::FullRefreshDirections::Down);
         }
