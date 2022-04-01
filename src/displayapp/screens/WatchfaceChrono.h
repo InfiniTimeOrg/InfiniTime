9ca16a75d59e3ba5ffadba628a9aa6e8f2fb2434
Jason Anderson <Jason.Anderson@jasonsblog.place>
2022-04-01 14:38:59 -0500

    Files for Chrono watchface from ZephyrLabs.

---
+++ src/displayapp/screens/WatchfaceChrono.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <lvgl/src/lv_core/lv_obj.h>
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include "displayapp/screens/Screen.h"
+#include "components/datetime/DateTimeController.h"
+
+namespace Pinetime {
+  namespace Controllers {
+    class Battery;
+    class Ble;
+    class NotificationManager;
+  }
+
+  namespace Applications {
+    namespace Screens {
+
+      template <class T>
+      class DirtyValue {
+        public:
+          explicit DirtyValue(T v) { value = v; }
+          explicit DirtyValue(T& v) { value = v; }
+          bool IsUpdated() const { return isUpdated; }
+          T& Get() { this->isUpdated = false; return value; }
+
+          DirtyValue& operator=(const T& other) {
+            if (this->value != other) {
+              this->value = other;
+              this->isUpdated = true;
+            }
+            return *this;
+          }
+        private:
+          T value;
+          bool isUpdated = true;
+      };
+      class Clock : public Screen {
+        public:
+          Clock(DisplayApp* app,
+                  Controllers::DateTime& dateTimeController,
+                  Controllers::Battery& batteryController,
+                  Controllers::Ble& bleController,
+                  Controllers::NotificationManager& notificatioManager);
+          ~Clock() override;
+
+          bool Refresh() override;
+          bool OnButtonPushed() override;
+          bool OnTouchEvent(TouchEvents event) override;
+        
+          void OnObjectEvent(lv_obj_t *pObj, lv_event_t i);
+        private:
+
+          void start();
+          void stop();
+          void reset();
+          float getCurrentTime();
+
+          float currentTime = 0.0f;
+          bool stopWatchRunning = false;
+        
+          std::chrono::system_clock::time_point startTime;
+
+          static const char* MonthToString(Pinetime::Controllers::DateTime::Months month);
+          static const char* DayOfWeekToString(Pinetime::Controllers::DateTime::Days dayOfWeek);
+
+          char displayedChar[5];
+
+          uint16_t currentYear = 1970;
+          Pinetime::Controllers::DateTime::Months currentMonth = Pinetime::Controllers::DateTime::Months::Unknown;
+          Pinetime::Controllers::DateTime::Days currentDayOfWeek = Pinetime::Controllers::DateTime::Days::Unknown;
+          uint8_t currentDay = 0;
+
+          DirtyValue<float> batteryPercentRemaining  {0};
+          DirtyValue<bool> bleState {false};
+          DirtyValue<std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>> currentDateTime;
+          DirtyValue<uint32_t> stepCount  {0};
+          DirtyValue<uint8_t> heartbeat  {0};
+          DirtyValue<bool> notificationState {false};
+
+          lv_obj_t* label_time;
+          lv_obj_t* label_date;
+          lv_obj_t* backgroundLabel;
+          lv_obj_t* batteryIcon;
+          lv_obj_t* bleIcon;
+          lv_obj_t* batteryPlug;
+          lv_obj_t* heartbeatIcon;
+          lv_obj_t* heartbeatValue;
+          lv_obj_t* heartbeatBpm;
+          lv_obj_t* stepIcon;
+          lv_obj_t* stepValue;
+          lv_obj_t* notificationIcon;
+
+          Controllers::DateTime& dateTimeController;
+          Controllers::Battery& batteryController;
+          Controllers::Ble& bleController;
+          Controllers::NotificationManager& notificatioManager;
+
+          bool running = true;
+
+      };
+    }
+  }
+}
