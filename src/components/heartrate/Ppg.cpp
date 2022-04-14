254fb846ce1bf577fda40c0d13697d9f7c0de9ce
watertrainer <watertrainer42@gmail.com>
2022-04-14 23:04:45 +0000

    ring buffer impl for processing of hr sensor data

--- src/components/heartrate/Ppg.cpp
+++ src/components/heartrate/Ppg.cpp
@@ -10,21 +10,25 @@
 using namespace Pinetime::Controllers;
 
 /** Original implementation from wasp-os : https://github.com/daniel-thompson/wasp-os/blob/master/wasp/ppg.py */
-namespace {
-  int Compare(int8_t* d1, int8_t* d2, size_t count) {
+Ppg::Ppg()
+  : hpf {0.87033078, -1.74066156, 0.87033078, -1.72377617, 0.75754694},
+    agc {20, 0.971, 2},
+    lpf {0.11595249, 0.23190498, 0.11595249, -0.72168143, 0.18549138} {
+}
+
+  int Ppg::Compare(int8_t* d1,int shift, size_t count) {
     int e = 0;
     for (size_t i = 0; i < count; i++) {
-      auto d = d1[i] - d2[i];
+      auto d = d1[getRingIndex(i+shift)] - d1[getRingIndex(i)];
       e += d * d;
     }
     return e;
   }
 
-  int CompareShift(int8_t* d, int shift, size_t count) {
-    return Compare(d + shift, d, count - shift);
+  int Ppg::CompareShift(int8_t* d, int shift, size_t count) {
+    return Compare(d ,shift, count - shift);
   }
-
-  int Trough(int8_t* d, size_t size, uint8_t mn, uint8_t mx) {
+  int Ppg::Trough(int8_t* d, size_t size, uint8_t mn, uint8_t mx) {
     auto z2 = CompareShift(d, mn - 2, size);
     auto z1 = CompareShift(d, mn - 1, size);
     for (int i = mn; i < mx + 1; i++) {
@@ -36,13 +40,6 @@ namespace {
     }
     return -1;
   }
-}
-
-Ppg::Ppg()
-  : hpf {0.87033078, -1.74066156, 0.87033078, -1.72377617, 0.75754694},
-    agc {20, 0.971, 2},
-    lpf {0.11595249, 0.23190498, 0.11595249, -0.72168143, 0.18549138} {
-}
 
 int8_t Ppg::Preprocess(float spl) {
   spl -= offset;
@@ -52,43 +49,49 @@ int8_t Ppg::Preprocess(float spl) {
 
   auto spl_int = static_cast<int8_t>(spl);
 
-  if (dataIndex < DATA_SIZE)
-    data[dataIndex++] = spl_int;
+  data[dataIndex] = spl_int;
+  dataIndex = (dataIndex+1)%Ppg::DATA_SIZE;
   return spl_int;
 }
 
 float Ppg::HeartRate() {
-  if (dataIndex < DATA_SIZE)
+  if(data[DATA_SIZE-1] == 0){
     return 0;
+  }
 
   NRF_LOG_INFO("PREPROCESS, offset = %d", offset);
   auto hr = ProcessHeartRate();
-  dataIndex = 0;
   return hr;
 }
 float Ppg::ProcessHeartRate() {
-  auto t0 = Trough(data.data(), DATA_SIZE, 7, 48);
+  int t0 = Trough(data.data(), DATA_SIZE, 7, 48);
   if (t0 < 0)
     return 0;
 
-  float t1 = t0 * 2;
-  t1 = Trough(data.data(), DATA_SIZE, t1 - 5, t1 + 5);
+  int t1 = t0 * 2;
+  t1 = Trough(data.data(), DATA_SIZE, (t1 - 5), (t1 + 5));
   if (t1 < 0)
     return 0;
 
-  float t2 = static_cast<int>(t1 * 3) / 2;
-  t2 = Trough(data.data(), DATA_SIZE, t2 - 5, t2 + 5);
+  int t2 = (t1 * 3) / 2;
+  t2 = Trough(data.data(), DATA_SIZE,(t2 - 5),(t2 + 5));
   if (t2 < 0)
     return 0;
 
-  float t3 = static_cast<int>(t2 * 4) / 3;
-  t3 = Trough(data.data(), DATA_SIZE, t3 - 4, t3 + 4);
+  int t3 = (t2 * 4) / 3;
+  t3 = Trough(data.data(), DATA_SIZE,(t3 - 4), (t3 + 4));
   if (t3 < 0)
-    return static_cast<int>(60 * 24 * 3) / static_cast<int>(t2);
+    return (60 * 24 * 3) / (t2);
 
-  return static_cast<int>(60 * 24 * 4) / static_cast<int>(t3);
+  return (60 * 24 * 4) / (t3);
 }
 
+//Gets the Index in the Ring Buffer which corresponds to the index, if it was a 0-based Array
+//DataIndex is where there was last data written to, so if the arrray was full once, it marks the end of the array
+//The next Index is the start then.
+int Ppg::getRingIndex(int8_t index){
+  return (index+dataIndex)%Ppg::DATA_SIZE;
+}
 void Ppg::SetOffset(uint16_t offset) {
   this->offset = offset;
   dataIndex = 0;
