94ee68b04d0c4e72d4d023cd8a6de02a0d79b444
Michele Bini <michele.bini@gmail.com>
2022-04-17 13:10:44 +0200

    Switch MotorController to FreeRTOS timers

--- src/components/motor/MotorController.cpp
+++ src/components/motor/MotorController.cpp
@@ -4,40 +4,38 @@
 #include "app_timer.h"
 #include "drivers/PinMap.h"
 
-APP_TIMER_DEF(shortVibTimer);
-APP_TIMER_DEF(longVibTimer);
-
 using namespace Pinetime::Controllers;
 
 void MotorController::Init() {
   nrf_gpio_cfg_output(PinMap::Motor);
   nrf_gpio_pin_set(PinMap::Motor);
-  app_timer_init();
 
-  app_timer_create(&shortVibTimer, APP_TIMER_MODE_SINGLE_SHOT, StopMotor);
-  app_timer_create(&longVibTimer, APP_TIMER_MODE_REPEATED, Ring);
+  shortVibTimer = xTimerCreate("shortVibTm", 1, pdFALSE, nullptr, StopMotor);
+  longVibTimer = xTimerCreate("longVibTm", 1, pdTRUE, this, Ring);
 }
 
-void MotorController::Ring(void* p_context) {
-  auto* motorController = static_cast<MotorController*>(p_context);
+void MotorController::Ring(TimerHandle_t xTimer) {
+  auto motorController = static_cast<MotorController*>(pvTimerGetTimerID(xTimer));
+  xTimerChangePeriod(motorController->longVibTimer, APP_TIMER_TICKS(1000), 0);
   motorController->RunForDuration(50);
 }
 
 void MotorController::RunForDuration(uint8_t motorDuration) {
-  nrf_gpio_pin_clear(PinMap::Motor);
-  app_timer_start(shortVibTimer, APP_TIMER_TICKS(motorDuration), nullptr);
+  if (xTimerChangePeriod(shortVibTimer, pdMS_TO_TICKS(motorDuration), 0) == pdPASS && xTimerStart(shortVibTimer, 0) == pdPASS) {
+    nrf_gpio_pin_clear(PinMap::Motor);
+  }
 }
 
 void MotorController::StartRinging() {
-  Ring(this);
-  app_timer_start(longVibTimer, APP_TIMER_TICKS(1000), this);
+  xTimerChangePeriod(longVibTimer, 1, 0);
+  xTimerStart(longVibTimer, 0);
 }
 
 void MotorController::StopRinging() {
-  app_timer_stop(longVibTimer);
+  xTimerStop(longVibTimer, 0);
   nrf_gpio_pin_set(PinMap::Motor);
 }
 
-void MotorController::StopMotor(void* p_context) {
+void MotorController::StopMotor(TimerHandle_t xTimer) {
   nrf_gpio_pin_set(PinMap::Motor);
 }
