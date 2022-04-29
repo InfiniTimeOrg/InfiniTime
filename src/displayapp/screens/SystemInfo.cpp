bf9e239a9adf1789f64ecc84fe739fdf70367143
ShimonHoranek <simonhoranek@gmail.com>
2022-04-29 15:54:11 +0200

    Odstranění diakritiky, zpáteční přidání nové stránky a oprava v knihovně

--- src/displayapp/screens/SystemInfo.cpp
+++ src/displayapp/screens/SystemInfo.cpp
@@ -218,7 +218,7 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen4() {
   lv_table_set_col_width(infoTask, 0, 30);
   lv_table_set_cell_value(infoTask, 0, 1, "S"); // State
   lv_table_set_col_width(infoTask, 1, 30);
-  lv_table_set_cell_value(infoTask, 0, 2, "Úloha");
+  lv_table_set_cell_value(infoTask, 0, 2, "Uloha");
   lv_table_set_col_width(infoTask, 2, 80);
   lv_table_set_cell_value(infoTask, 0, 3, "Volno");
   lv_table_set_col_width(infoTask, 3, 90);
@@ -281,6 +281,18 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen5() {
   lv_label_set_align(label, LV_LABEL_ALIGN_CENTER);
   lv_obj_align(label, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
   return std::make_unique<Screens::Label>(4, 6, app, label);
+  
 }
-
+std::unique_ptr<Screen> SystemInfo::CreateScreen6() {
+  lv_obj_t* label = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_recolor(label, true);
+  lv_label_set_text_static(label,
+			   "#444444 Cesky preklad#\n"
+                           "#FFFF00 https://github.com/#\n"
+                           "#FFFF00 ShimonHoranek/#\n"
+                           "#FFFF00 InfiniTime#\n");
+  lv_label_set_align(label, LV_LABEL_ALIGN_CENTER);
+  lv_obj_align(label, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+  return std::make_unique<Screens::Label>(5, 6, app, label);
+}
 
--- src/displayapp/screens/SystemInfo.h
+++ src/displayapp/screens/SystemInfo.h
@@ -42,7 +42,7 @@ namespace Pinetime {
         Pinetime::Controllers::MotionController& motionController;
         Pinetime::Drivers::Cst816S& touchPanel;
 
-        ScreenList<5> screens;
+        ScreenList<6> screens;
 
         static bool sortById(const TaskStatus_t& lhs, const TaskStatus_t& rhs);
 
@@ -51,6 +51,7 @@ namespace Pinetime {
         std::unique_ptr<Screen> CreateScreen3();
         std::unique_ptr<Screen> CreateScreen4();
         std::unique_ptr<Screen> CreateScreen5();
+        std::unique_ptr<Screen> CreateScreen6();
       };
     }
   }
