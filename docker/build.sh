94cd2ba563b236d8ba7eafdbced3e69d1bcfcfc6
Finlay Davidson <finlay.davidson@coderclass.nl>
2022-05-10 20:26:41 +0200

    Update arm gcc toolchain to 11.2-2022.02
    Updates the buildAndProgram doc and the docker
    image, which means GitHub Actions will also use
    the new toolchain.

--- docker/build.sh
+++ docker/build.sh
@@ -12,18 +12,20 @@ export BUILD_DIR="${BUILD_DIR:=$SOURCES_DIR/build}"
 export OUTPUT_DIR="${OUTPUT_DIR:=$SOURCES_DIR/build/output}"
 
 export BUILD_TYPE=${BUILD_TYPE:=Release}
-export GCC_ARM_VER=${GCC_ARM_VER:="gcc-arm-none-eabi-9-2020-q2-update"}
+export GCC_ARM_VER=${GCC_ARM_VER:="11.2-2022.02"}
 export NRF_SDK_VER=${NRF_SDK_VER:="nRF5_SDK_15.3.0_59ac345"}
 
 MACHINE="$(uname -m)"
 [[ "$MACHINE" == "arm64" ]] && MACHINE="aarch64"
+
+export GCC_ARM_PATH="gcc-arm-$GCC_ARM_VER-$MACHINE-arm-none-eabi"
 
 main() {
   local target="$1"
 
   mkdir -p "$TOOLS_DIR"
 
-  [[ ! -d "$TOOLS_DIR/$GCC_ARM_VER" ]] && GetGcc
+  [[ ! -d "$TOOLS_DIR/$GCC_ARM_PATH" ]] && GetGcc
   [[ ! -d "$TOOLS_DIR/$NRF_SDK_VER" ]] && GetNrfSdk
   [[ ! -d "$TOOLS_DIR/mcuboot" ]] && GetMcuBoot
 
@@ -38,8 +40,7 @@ main() {
 }
 
 GetGcc() {
-  GCC_SRC="$GCC_ARM_VER-$MACHINE-linux.tar.bz"
-  wget -q https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/$GCC_SRC -O - | tar -xj -C $TOOLS_DIR/
+  wget -q https://developer.arm.com/-/media/Files/downloads/gnu/$GCC_ARM_VER/binrel/$GCC_ARM_PATH.tar.xz -O - | tar -xJ -C $TOOLS_DIR/
 }
 
 GetMcuBoot() {
@@ -59,7 +60,7 @@ CmakeGenerate() {
     -B "$BUILD_DIR" \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DUSE_OPENOCD=1 \
-    -DARM_NONE_EABI_TOOLCHAIN_PATH="$TOOLS_DIR/$GCC_ARM_VER" \
+    -DARM_NONE_EABI_TOOLCHAIN_PATH="$TOOLS_DIR/$GCC_ARM_PATH" \
     -DNRF5_SDK_PATH="$TOOLS_DIR/$NRF_SDK_VER" \
     -DBUILD_DFU=1
 }
