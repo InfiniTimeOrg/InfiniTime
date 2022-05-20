fd43df167e7ea49e468fc14515b6b76465fbe39a
Riku Isokoski <riksu9000@gmail.com>
2022-05-20 11:55:40 +0300

    Automatically create enough screens for all apps and settings

--- src/displayapp/screens/settings/Settings.h
+++ src/displayapp/screens/settings/Settings.h
@@ -3,6 +3,8 @@
 #include <cstdint>
 #include "displayapp/screens/Screen.h"
 #include "displayapp/screens/ScreenList.h"
+#include "displayapp/screens/Symbols.h"
+#include "displayapp/screens/List.h"
 
 namespace Pinetime {
 
@@ -17,14 +19,34 @@ namespace Pinetime {
         bool OnTouchEvent(Pinetime::Applications::TouchEvents event) override;
 
       private:
+        auto CreateScreenList() const;
+        std::unique_ptr<Screen> CreateScreen(unsigned int screenNum) const;
+
         Controllers::Settings& settingsController;
 
-        ScreenList<4> screens;
+        static constexpr std::array<List::Applications, 16> entries {{
+          {Symbols::sun, "Display", Apps::SettingDisplay},
+          {Symbols::eye, "Wake Up", Apps::SettingWakeUp},
+          {Symbols::clock, "Time format", Apps::SettingTimeFormat},
+          {Symbols::home, "Watch face", Apps::SettingWatchFace},
 
-        std::unique_ptr<Screen> CreateScreen1();
-        std::unique_ptr<Screen> CreateScreen2();
-        std::unique_ptr<Screen> CreateScreen3();
-        std::unique_ptr<Screen> CreateScreen4();
+          {Symbols::shoe, "Steps", Apps::SettingSteps},
+          {Symbols::clock, "Set date", Apps::SettingSetDate},
+          {Symbols::clock, "Set time", Apps::SettingSetTime},
+          {Symbols::batteryHalf, "Battery", Apps::BatteryInfo},
+
+          {Symbols::clock, "Chimes", Apps::SettingChimes},
+          {Symbols::tachometer, "Shake Calib.", Apps::SettingShakeThreshold},
+          {Symbols::check, "Firmware", Apps::FirmwareValidation},
+          {Symbols::bluetooth, "Bluetooth", Apps::SettingBluetooth},
+
+          {Symbols::list, "About", Apps::SysInfo},
+          {Symbols::none, "None", Apps::None},
+          {Symbols::none, "None", Apps::None},
+          {Symbols::none, "None", Apps::None},
+        }};
+        static constexpr int nScreens = (entries.size() + 3) / 4;
+        ScreenList<nScreens> screens;
       };
     }
   }
