e3558e704f36dfd9c814821d790220ee0d86f4a8
0x0000ff <0x0000ff@github.com>
2022-06-22 11:51:01 +0100

    Added Icons to counter buttons.

--- src/displayapp/screens/Symbols.h
+++ src/displayapp/screens/Symbols.h
@@ -37,6 +37,8 @@ namespace Pinetime {
         static constexpr const char* chartLine = "\xEF\x88\x81";
         static constexpr const char* eye = "\xEF\x81\xAE";
         static constexpr const char* home = "\xEF\x80\x95";
+        static constexpr const char* upArrow = "\xEF\x83\x98";
+        static constexpr const char* downArrow = "\xEF\x83\x97";
 
         // lv_font_sys_48.c
         static constexpr const char* settings = "\xEE\xA4\x82"; // e902
