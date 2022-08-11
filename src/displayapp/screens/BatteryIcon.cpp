a091e57c0b43b46b9f229e1f647ac79dce04ff69
mashuptwice <info@mashup-tech.de>
2022-08-11 23:10:28 +0200

    interactive colored battery icon

--- src/displayapp/screens/BatteryIcon.cpp
+++ src/displayapp/screens/BatteryIcon.cpp
@@ -23,6 +23,13 @@ lv_obj_t* BatteryIcon::GetObject() {
 void BatteryIcon::SetBatteryPercentage(uint8_t percentage) {
   lv_obj_set_height(batteryJuice, percentage * 14 / 100);
   lv_obj_realign(batteryJuice);
+  if (percentage <= 40) {
+  lv_obj_set_style_local_bg_color(batteryJuice, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0xFF0000));
+    } else if (percentage > 40 && percentage < 60) {
+  lv_obj_set_style_local_bg_color(batteryJuice, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0xFF8600));
+    } else if (percentage >= 60) {
+  lv_obj_set_style_local_bg_color(batteryJuice, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x24FF00));
+    }
 }
 
 void BatteryIcon::SetColor(lv_color_t color) {
