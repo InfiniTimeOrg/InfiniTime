d11fc65bfca995ab25bc330fe62d96446e215865
minacode <minamoto9@web.de>
2022-08-23 02:46:18 +0200

    add apps config file

---
+++ tools/apps_prebuild.py
@@ -0,0 +1,57 @@
+import math
+
+symbols = {
+    'Alarm': 'clock',
+    'HeartRate': 'heartBeat',
+    'Metronome': 'drum',
+    'Motion': 'chartLine',
+    'Music': 'music',
+    'Navigation': 'map',
+    'Paddle': 'paddle',
+    'Paint': 'paintbrush',
+    'Steps': 'shoe',
+    'StopWatch': 'stopWatch',
+    'Timer': 'hourGlass',
+    'Twos': '2',
+}
+
+with open('apps.txt', 'r') as file:
+    apps = file.readlines()
+
+apps = [app.strip() for app in apps]
+
+print('Apps:')
+print(apps)
+
+lines = []
+for app in apps:
+    if app in symbols:
+        if app == 'Twos':
+            symbol = '"2"'
+        else:
+            symbol = f'Symbols::{symbols[app]}'
+        # 10 spaces for correct indentation
+        # '{' and '}' separate because format strings interfere with them
+        # Symbols::symbol, Apps::app in the middle
+        # \n at the end for correct format
+        lines.append(' '*10 + '{' + f'{symbol}, Apps::{app}'  + '},\n')
+
+# \n at start
+# 8 space for correct indentation of closing braces
+lines = '\n' + ''.join(lines) + ' '*8
+
+print(lines)
+
+# 6 apps per screen, result rounded up
+n_screens = math.ceil(len(lines) / 6)
+
+with open('tools/template.h', 'r') as template:
+    content = template.read()
+
+content = content.replace('@N_SCREENS@', str(n_screens))
+content = content.replace('@APPS@', lines)
+
+with open('src/displayapp/screens/ApplicationList.h', 'w') as out:
+    out.write(content)
+
+print('Done.')
