12a565a5763111753b651f2fa3495b04960ed254
yusuf ebrahim <yusufmte@gmail.com>
2022-09-11 00:16:46 -0400

    add dice symbol

--- src/displayapp/screens/Symbols.h
+++ src/displayapp/screens/Symbols.h
@@ -34,6 +34,7 @@ namespace Pinetime {
         static constexpr const char* hourGlass = "\xEF\x89\x92";
         static constexpr const char* lapsFlag = "\xEF\x80\xA4";
         static constexpr const char* drum = "\xEF\x95\xA9";
+        static constexpr const char* dice = "\xEF\x94\xA2";
         static constexpr const char* chartLine = "\xEF\x88\x81";
         static constexpr const char* eye = "\xEF\x81\xAE";
         static constexpr const char* home = "\xEF\x80\x95";
