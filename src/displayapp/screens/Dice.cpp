f00d779d3e6871c185f5e437ce9255bc440d92af
yusuf ebrahim <yusufmte@gmail.com>
2022-09-11 22:55:11 -0400

    modify dice seed to use epoch time

--- src/displayapp/screens/Dice.cpp
+++ src/displayapp/screens/Dice.cpp
@@ -16,7 +16,7 @@ static void btnRollEventHandler(lv_obj_t* obj, lv_event_t event) {
 
 Dice::Dice(DisplayApp* app, Controllers::DateTime& dateTime) : Screen(app), dateTime {dateTime} {
 
-  srand(dateTime.Uptime().count() * dateTime.Seconds());
+  srand(dateTime.Uptime().count() * time(nullptr));
 
   lv_obj_t* dLabel = lv_label_create(lv_scr_act(), nullptr);
   lv_obj_set_style_local_text_font(dLabel, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &jetbrains_mono_42);
