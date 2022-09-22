59746c41007e51a83090a3dc9c25f3765de0500f
minacode <minamoto9@web.de>
2022-09-22 14:12:38 +0200

    add missing file

---
+++ src/displayapp/WatchFaces.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Pinetime {
+  namespace Applications {
+    enum class WatchFace : uint8_t {
+      Digital,
+      Analog,
+      PineTimeStyle,
+      Terminal,
+    };
+  }
+}
