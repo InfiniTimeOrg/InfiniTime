bb77ea9623cd6e39da88d1f7bc0a5aadd9544d7f
minacode <minamoto9@web.de>
2022-10-03 14:22:18 +0200

    add percentage rescaling

--- src/components/battery/BatteryController.cpp
+++ src/components/battery/BatteryController.cpp
@@ -82,6 +82,11 @@ void Battery::SaadcEventHandler(nrfx_saadc_evt_t const* p_event) {
     } else {
       newPercent = std::min((voltage - battery_min) * 100 / (battery_max - battery_min), isCharging ? 99 : 100);
     }
+    // quick hack for better values
+    // rescale the percentages between 35 and 100
+    constexpr uint8_t realMin = 35;
+    newPercent = std::max(newPercent, realMin);
+    newPercent = (newPercent - realMin) * 100 / (100 - realMin);
 
     if ((isPowerPresent && newPercent > percentRemaining) || (!isPowerPresent && newPercent < percentRemaining) || firstMeasurement) {
       firstMeasurement = false;
@@ -90,7 +95,8 @@ void Battery::SaadcEventHandler(nrfx_saadc_evt_t const* p_event) {
       systemTask->PushMessage(System::Messages::BatteryPercentageUpdated);
     }
 
-    constexpr uint8_t lowBatteryThreshold {50};
+    // warn at 20% battery (wrt. rescaling above)
+    constexpr uint8_t lowBatteryThreshold {20};
     if (!isPowerPresent && lastPercentRemaining >= lowBatteryThreshold && percentRemaining < lowBatteryThreshold) {
       systemTask->PushMessage(System::Messages::LowBattery);
     }
