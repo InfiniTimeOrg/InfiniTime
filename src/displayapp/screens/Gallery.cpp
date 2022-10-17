2034cc9be36f80b3527ad1c196a7f66932536f8b
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2022-10-17 22:13:08 +0100

    Fixed OBOE in open

--- src/displayapp/screens/Gallery.cpp
+++ src/displayapp/screens/Gallery.cpp
@@ -52,18 +52,14 @@ bool Gallery::open(int n, DisplayApp::FullRefreshDirections direction) {
     NRF_LOG_INFO("[Gallery] can't find directory");
     return false;
   }
-  while (filesystem.DirRead(&dir, &info) && n > 0) {
+  int i = 0;
+  while (filesystem.DirRead(&dir, &info)) {
     if(info.type == LFS_TYPE_DIR)
       continue;
-    n--;
+    if (n == i) break;
+    i++;
   }
   assert(filesystem.DirClose(&dir) == 0);
-
-  lfs_file f = {0};
-  if (filesystem.FileOpen(&f, info.name, LFS_O_RDONLY) != LFS_ERR_OK)
-    return false;
-
-  assert(filesystem.FileClose(&f) == 0);
 
   if (current != nullptr) {
     current.reset(nullptr);
