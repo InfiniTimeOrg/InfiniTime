a619ff86347de276df74b90bffafe9cd43c4ec37
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2022-10-17 21:19:43 +0100

    Added ImageView

---
+++ src/displayapp/screens/ImageView.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "displayapp/screens/Screen.h"
+#include "displayapp/DisplayApp.h"
+#include <lvgl/lvgl.h>
+
+namespace Pinetime {
+  namespace Applications {
+    namespace Screens {
+      class ImageView : public Screen {
+      public:
+        ImageView(DisplayApp* app);
+        ~ImageView() override;
+      };
+    }
+  }
+}
+
