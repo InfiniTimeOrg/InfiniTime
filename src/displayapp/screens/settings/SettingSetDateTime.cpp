2a0ae5872063cb109e232a1df3424dedbe48838a
Elements6007 <elements6007@gmail.com>
2022-10-22 22:46:39 -0400

    Clang-format

--- src/displayapp/screens/settings/SettingSetDateTime.cpp
+++ src/displayapp/screens/settings/SettingSetDateTime.cpp
@@ -6,7 +6,7 @@
 #include "displayapp/screens/Symbols.h"
 #include "displayapp/screens/ScreenList.h"
 #include "displayapp/screens/Label.h"
-//SettingSetTime
+// SettingSetTime
 #include "components/settings/Settings.h"
 #include "displayapp/InfiniTimeTheme.h"
 
@@ -46,13 +46,13 @@ namespace {
         return 31;
     }
   }
-  //SettingSetTime
+  // SettingSetTime
   constexpr int16_t POS_Y_TEXTD = -7;
 
   void SetTimeEventHandler(lv_obj_t* obj, lv_event_t event) {
-     auto* screen = static_cast<SettingSetDateTime*>(obj->user_data);
-     if (event == LV_EVENT_CLICKED) {
-     screen->SetTime();
+    auto* screen = static_cast<SettingSetDateTime*>(obj->user_data);
+    if (event == LV_EVENT_CLICKED) {
+      screen->SetTime();
     }
   }
 
@@ -66,9 +66,13 @@ bool SettingSetDateTime::OnTouchEvent(Pinetime::Applications::TouchEvents event)
   return screens.OnTouchEvent(event);
 }
 
-SettingSetDateTime::SettingSetDateTime(Pinetime::Applications::DisplayApp* app, Pinetime::Controllers::DateTime& dateTimeController,
-                               Pinetime::Controllers::Settings& settingsController)
-  : Screen(app), dateTimeController {dateTimeController}, settingsController {settingsController}, screens {app,
+SettingSetDateTime::SettingSetDateTime(Pinetime::Applications::DisplayApp* app,
+                                       Pinetime::Controllers::DateTime& dateTimeController,
+                                       Pinetime::Controllers::Settings& settingsController)
+  : Screen(app),
+    dateTimeController {dateTimeController},
+    settingsController {settingsController},
+    screens {app,
              0,
              {[this]() -> std::unique_ptr<Screen> {
                 return CreateScreen1();
@@ -77,7 +81,7 @@ SettingSetDateTime::SettingSetDateTime(Pinetime::Applications::DisplayApp* app, 
                 return CreateScreen2();
               }},
              Screens::ScreenListModes::UpDown} {
-}  
+}
 
 std::unique_ptr<Screen> SettingSetDateTime::CreateScreen1() {
 
@@ -121,16 +125,15 @@ std::unique_ptr<Screen> SettingSetDateTime::CreateScreen1() {
   lv_obj_set_state(lblSetDate, LV_STATE_DISABLED);
 
   return std::make_unique<Screens::Label>(0, 2, app, title);
-
 }
 
 SettingSetDateTime::~SettingSetDateTime() {
   lv_obj_clean(lv_scr_act());
 }
 
-//Screen two    SettingSetTime
+// Screen two    SettingSetTime
 std::unique_ptr<Screen> SettingSetDateTime::CreateScreen2() {
- 
+
   lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_static(title, "Set current time");
   lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
@@ -205,7 +208,7 @@ void SettingSetDateTime::CheckDay() {
   lv_btn_set_state(btnSetDate, LV_BTN_STATE_RELEASED);
   lv_obj_set_state(lblSetDate, LV_STATE_DEFAULT);
 }
-//SettingSetTime
+// SettingSetTime
 void SettingSetDateTime::UpdateScreen() {
   if (settingsController.GetClockType() == Controllers::Settings::ClockType::H12) {
     if (hourCounter.GetValue() >= 12) {
