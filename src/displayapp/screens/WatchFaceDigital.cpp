48b3850d761816deb6d5b8985f0821b545e64662
0cc4m <picard12@live.de>
2022-10-23 20:43:40 +0200

    Rename notificatioManager to notificationManager
    Co-authored-by: Commenter <35016761+Commenter25@users.noreply.github.com>

--- src/displayapp/screens/WatchFaceDigital.cpp
+++ src/displayapp/screens/WatchFaceDigital.cpp
@@ -88,7 +88,7 @@ void WatchFaceDigital::Refresh() {
     if (notificationState.Get()) {
       lv_obj_set_style_local_text_color(notificationIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0x00FF00));
       lv_label_set_text_static(notificationIcon, NotificationIcon::GetIcon(true));
-    } else if (notificatioManager.NbNotifications() > 0) {
+    } else if (notificationManager.NbNotifications() > 0) {
       lv_obj_set_style_local_text_color(notificationIcon, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0xFFFFFF));
       lv_label_set_text_static(notificationIcon, NotificationIcon::GetIcon(true));
     } else {
