9546275d22bc2a9667ee3f915d34c6dcef95dffd
Alex Dolzhenkov <admin@alexx.pw>
2022-10-29 12:20:44 +1300

    Add linear approximation and use it for improving battery percentage

---
+++ src/components/utility/LinearApproximation.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <array>    // for std::array
+#include <utility>  // for std::pair
+
+namespace Pinetime {
+  namespace Utility {
+
+    // based on: https://github.com/SHristov92/LinearApproximation/blob/main/Linear.h
+    template <typename Key, typename Value, std::size_t Size>
+    class LinearApproximation {
+      using Point = struct {
+        Key key;
+        Value value;
+      };
+
+    public:
+      LinearApproximation(const std::array<Point, Size> &&points)
+        : points{points} {}
+
+      Value GetValue(Key key) const {
+        if (key <= points[0].key) {
+          return points[0].value;
+        }
+
+        for (size_t i = 1; i < Size; i++) {
+          if (key < points[i].key) {
+            return points[i-1].value + (key - points[i-1].key) * (points[i].value - points[i-1].value) / (points[i].key - points[i-1].key);
+          }
+        }
+
+        return points[Size - 1].value;
+      }
+
+    private:
+      std::array<Point, Size> points;
+    };
+  }
+}
