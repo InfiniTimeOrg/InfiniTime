9c1e530581be9ad9b2079e7d451a8337a17b7e9b
Derek Briggs <contact@derekbriggs.xyz>
2022-10-29 02:15:03 -0400

    Finalize code for MTG Life Points app

--- src/displayapp/screens/MTGLifePoints.cpp
+++ src/displayapp/screens/MTGLifePoints.cpp
@@ -5,9 +5,12 @@ using namespace Pinetime::Applications::Screens;
 
 MTGLifePoints::MTGLifePoints(DisplayApp* app) : Screen(app) {
   lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "My test application");
+  lv_label_set_text_static(title, "Life Points:");
   lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
-  lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+  lv_obj_align(title, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 0, 0);
+
+  twoDigitCounter.Create();
+  lv_obj_align(twoDigitCounter.GetObject(), nullptr, LV_ALIGN_CENTER, 0, 0);
 }
 
 MTGLifePoints::~MTGLifePoints() {
