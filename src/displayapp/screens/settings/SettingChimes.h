80369f7e4ba0fef99fea34c6fbcfdeaf2627557f
Elements6007 <elements6007@gmail.com>
2022-10-30 08:52:52 -0400

    Clang format

--- src/displayapp/screens/settings/SettingChimes.h
+++ src/displayapp/screens/settings/SettingChimes.h
@@ -23,11 +23,9 @@ namespace Pinetime {
           Controllers::Settings::ChimesOption chimesOption;
           const char* name;
         };
-        static constexpr std::array<Option, 3> options = {{
-          {Controllers::Settings::ChimesOption::None, "Off"},
-          {Controllers::Settings::ChimesOption::Hours, "Every hour"},
-          {Controllers::Settings::ChimesOption::HalfHours, "Every 30 mins"}
-        }};
+        static constexpr std::array<Option, 3> options = {{{Controllers::Settings::ChimesOption::None, "Off"},
+                                                           {Controllers::Settings::ChimesOption::Hours, "Every hour"},
+                                                           {Controllers::Settings::ChimesOption::HalfHours, "Every 30 mins"}}};
 
         std::array<lv_obj_t*, options.size()> cbOption;
 
