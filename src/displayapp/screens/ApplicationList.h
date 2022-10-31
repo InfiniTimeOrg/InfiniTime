815e0f2a9a31aedb7fdc89a10271bde6291a45b2
Derek Briggs <contact@derekbriggs.xyz>
2022-10-31 00:58:56 -0400

    Remove yugioh app from app list on the Pinetime

--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -36,7 +36,7 @@ namespace Pinetime {
         static constexpr int appsPerScreen = 6;
 
         // Increment this when more space is needed
-        static constexpr int nScreens = 3;
+        static constexpr int nScreens = 2;
 
         static constexpr std::array<Tile::Applications, appsPerScreen * nScreens> applications {{
           {Symbols::stopWatch, Apps::StopWatch},
@@ -52,8 +52,6 @@ namespace Pinetime {
           {Symbols::chartLine, Apps::Motion},
           {Symbols::drum, Apps::Metronome},
           {Symbols::map, Apps::Navigation},
-
-          {"Y", Apps::YugiohLifePoints}
         }};
         ScreenList<nScreens> screens;
       };
