18a75dae84a929fbad97b5660b66397ec0b9fc8d
minacode <minamoto9@web.de>
2022-11-01 19:43:07 +0100

    fixed screen count

--- src/displayapp/AvailableApps.h
+++ src/displayapp/AvailableApps.h
@@ -1,7 +1,8 @@
-#define APP_MOTION     0
-#define APP_METRONOME  0
-#define APP_MUSIC      0
-#define APP_PADDLE     0
-#define APP_TWOS       0
-#define APP_NAVIGATION 0
-#define APP_PAINT      0
+#define APP_MOTION     1
+#define APP_METRONOME  1
+#define APP_MUSIC      1
+#define APP_PADDLE     1
+#define APP_TWOS       1
+#define APP_NAVIGATION 1
+#define APP_PAINT      1
+#define APP_WEATHER    0
--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -12,8 +12,7 @@
 #include "displayapp/screens/Symbols.h"
 #include "displayapp/screens/Tile.h"
 
-#define N_APPS    5 + APP_METRONOME + APP_MUSIC + APP_NAVIGATION + APP_PADDLE + APP_PAINT + APP_TWOS + APP_MOTION
-#define N_SCREENS ((N_APPS - N_APPS % 6) / 6) + (N_APPS % 6 == 0 ? 0 : 1)
+#define N_APPS (5 + APP_METRONOME + APP_MUSIC + APP_NAVIGATION + APP_PADDLE + APP_PAINT + APP_TWOS + APP_MOTION)
 
 namespace Pinetime {
   namespace Applications {
@@ -37,9 +36,8 @@ namespace Pinetime {
         Pinetime::Controllers::Ble& bleController;
         Controllers::DateTime& dateTimeController;
 
-        static constexpr int appsPerScreen = 6;
-
-        static constexpr int nScreens = N_SCREENS;
+        static constexpr int appsPerScreen {6};
+        static constexpr int nScreens {(N_APPS / 6) + ((N_APPS % 6) != 0)};
 
         static constexpr std::array<Tile::Applications, appsPerScreen * nScreens> applications {{
           {Symbols::stopWatch, Apps::StopWatch},
