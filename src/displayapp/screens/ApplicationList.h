4f33b0fc820aeffcecb03300f794fad7e346e690
minacode <minamoto9@web.de>
2022-11-11 19:10:41 +0100

    fixed application list initialization

--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -16,6 +16,41 @@
 namespace Pinetime {
   namespace Applications {
     namespace Screens {
+      template <size_t SIZE> constexpr std::array<Tile::Applications, SIZE> initApplications() {
+        std::array<Tile::Applications, SIZE> temp_applications {{
+          {Symbols::stopWatch, Apps::StopWatch},
+          {Symbols::clock, Apps::Alarm},
+          {Symbols::hourGlass, Apps::Timer},
+          {Symbols::shoe, Apps::Steps},
+          {Symbols::heartBeat, Apps::HeartRate},
+        }};
+      
+        auto i = 5;
+        if (APP_MUSIC) {
+          temp_applications[i++] = {Symbols::music, Apps::Music};
+        }
+        if (APP_PAINT) {
+          temp_applications[i++] = {Symbols::paintbrush, Apps::Paint};
+        }
+        if (APP_PADDLE) {
+          temp_applications[i++] = {Symbols::paddle, Apps::Paddle};
+        }
+        if (APP_TWOS) {
+          temp_applications[i++] = {"2", Apps::Twos};
+        }
+        if (APP_MOTION) {
+          temp_applications[i++] = {Symbols::chartLine, Apps::Motion};
+        }
+        if (APP_METRONOME) {
+          temp_applications[i++] = {Symbols::drum, Apps::Metronome};
+        }
+        if (APP_NAVIGATION) {
+          temp_applications[i++] = {Symbols::map, Apps::Navigation};
+        }
+      
+        return temp_applications;
+      }
+
       class ApplicationList : public Screen {
       public:
         explicit ApplicationList(DisplayApp* app,
@@ -39,20 +74,8 @@ namespace Pinetime {
         static constexpr int nApps {5 + APP_METRONOME + APP_MUSIC + APP_NAVIGATION + APP_PADDLE + APP_PAINT + APP_TWOS + APP_MOTION};
         static constexpr int nScreens {(nApps / appsPerScreen) + ((nApps % appsPerScreen) != 0)};
 
-        static constexpr std::array<Tile::Applications, appsPerScreen * nScreens> applications {{
-          {Symbols::stopWatch, Apps::StopWatch},
-          {Symbols::clock, Apps::Alarm},
-          {Symbols::hourGlass, Apps::Timer},
-          {Symbols::shoe, Apps::Steps},
-          {Symbols::heartBeat, Apps::HeartRate},
-          APP_MUSIC ? (Tile::Applications) {Symbols::music, Apps::Music} : (Tile::Applications) {nullptr, Apps::None},
-          APP_PAINT ? (Tile::Applications) {Symbols::paintbrush, Apps::Paint} : (Tile::Applications) {nullptr, Apps::None},
-          APP_PADDLE ? (Tile::Applications) {Symbols::paddle, Apps::Paddle} : (Tile::Applications) {nullptr, Apps::None},
-          APP_TWOS ? (Tile::Applications) {"2", Apps::Twos} : (Tile::Applications) {nullptr, Apps::None},
-          APP_MOTION ? (Tile::Applications) {Symbols::chartLine, Apps::Motion} : (Tile::Applications) {nullptr, Apps::None},
-          APP_METRONOME ? (Tile::Applications) {Symbols::drum, Apps::Metronome} : (Tile::Applications) {nullptr, Apps::None},
-          APP_NAVIGATION ? (Tile::Applications) {Symbols::map, Apps::Navigation} : (Tile::Applications) {nullptr, Apps::None},
-        }};
+
+        static constexpr std::array<Tile::Applications, appsPerScreen * nScreens> applications = initApplications<appsPerScreen * nScreens>();
         ScreenList<nScreens> screens;
       };
     }
