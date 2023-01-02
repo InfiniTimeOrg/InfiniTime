822f857d9e3bdc38985286652bec0d9cce89297d
Riku Isokoski <riksu9000@gmail.com>
2023-01-02 10:34:50 +0200

    stopwatch: Update UI
    The time used to be yellow while paused. Changing it to white made the
    paused state less distinct. Blinking the time while paused makes the
    state distinct again.

--- src/displayapp/InfiniTimeTheme.h
+++ src/displayapp/InfiniTimeTheme.h
@@ -5,6 +5,7 @@
 namespace Colors {
   static constexpr lv_color_t orange = LV_COLOR_MAKE(0xff, 0xb0, 0x0);
   static constexpr lv_color_t green = LV_COLOR_MAKE(0x0, 0xb0, 0x0);
+  static constexpr lv_color_t blue = LV_COLOR_MAKE(0x0, 0x50, 0xff);
   static constexpr lv_color_t lightGray = LV_COLOR_MAKE(0xb0, 0xb0, 0xb0);
 
   static constexpr lv_color_t bg = LV_COLOR_MAKE(0x5d, 0x69, 0x7e);
