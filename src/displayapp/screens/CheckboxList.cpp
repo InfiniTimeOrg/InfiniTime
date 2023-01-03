68d0df4731bc499430413d21c325cab45709247c
Riku Isokoski <riksu9000@gmail.com>
2023-01-03 17:09:59 +0200

    SetingTimeFormat use CheckboxList, reduce duplication

--- src/displayapp/screens/CheckboxList.cpp
+++ src/displayapp/screens/CheckboxList.cpp
@@ -28,7 +28,9 @@ CheckboxList::CheckboxList(const uint8_t screenID,
   // Set the background to Black
   lv_obj_set_style_local_bg_color(lv_scr_act(), LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_BLACK);
 
-  pageIndicator.Create();
+  if (numScreens > 1) {
+    pageIndicator.Create();
+  }
 
   lv_obj_t* container1 = lv_cont_create(lv_scr_act(), nullptr);
 
