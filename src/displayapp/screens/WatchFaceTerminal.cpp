ea59d4fb180d0e10cb27d25f27628913b6f66072
Fuzzy Express <127012288+FuzzyExpress@users.noreply.github.com>
2023-03-12 00:54:56 -0600

    Update WatchFaceTerminal.cpp

--- src/displayapp/screens/WatchFaceTerminal.cpp
+++ src/displayapp/screens/WatchFaceTerminal.cpp
@@ -46,9 +46,9 @@ WatchFaceTerminal::WatchFaceTerminal(Controllers::DateTime& dateTimeController,
   lv_obj_align(label_prompt_1, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 0, -80);
   lv_label_set_text_static(label_prompt_1, "evan@watch:~$ now");
 
- # label_prompt_2 = lv_label_create(lv_scr_act(), nullptr);
- # lv_obj_align(label_prompt_2, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 0, 60);
- # lv_label_set_text_static(label_prompt_2, "user@watch:~ $");
+ // label_prompt_2 = lv_label_create(lv_scr_act(), nullptr);
+ // lv_obj_align(label_prompt_2, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 0, 60);
+ // lv_label_set_text_static(label_prompt_2, "evan@watch:~$");
 
   label_time = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_recolor(label_time, true);
