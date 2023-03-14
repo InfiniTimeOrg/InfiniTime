ff5b734794a48cb7f218ba733d6f879f1b39f447
Fuzzy Express <127012288+FuzzyExpress@users.noreply.github.com>
2023-03-14 11:17:58 -0500

    Update WatchFaceAnalog.h

--- src/displayapp/screens/WatchFaceAnalog.h
+++ src/displayapp/screens/WatchFaceAnalog.h
@@ -47,6 +47,9 @@ namespace Pinetime {
         DirtyValue<std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>> currentDateTime;
         DirtyValue<bool> notificationState {false};
 
+        lv_obj_t* backdropmain;
+        lv_style_t backdrop;
+        
         lv_obj_t* hour_body;
         lv_obj_t* hour_body_trace;
         lv_obj_t* minute_body;
