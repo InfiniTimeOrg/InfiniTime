9e3be1d98730582134df305bdaa91a46f5f65a73
Brod8362 <brod8362@gmail.com>
2023-03-17 17:22:04 -0500

    Add notification icons to Symbols

--- src/displayapp/screens/NotificationIcon.cpp
+++ src/displayapp/screens/NotificationIcon.cpp
@@ -13,7 +13,28 @@ const char* NotificationIcon::GetIcon(bool newNotificationAvailable) {
 const char* NotificationIcon::GetCategoryIcon(Pinetime::Controllers::NotificationManager::Categories category) {
   switch (category) {
     //TODO add symbols for each category
+    case Pinetime::Controllers::NotificationManager::Categories::SimpleAlert:
+      return Symbols::bell;
+    case Pinetime::Controllers::NotificationManager::Categories::Email:
+      return Symbols::envelope;
+    case Pinetime::Controllers::NotificationManager::Categories::News:
+      return Symbols::newspaper;
+    case Pinetime::Controllers::NotificationManager::Categories::IncomingCall:
+      return Symbols::phone;
+    case Pinetime::Controllers::NotificationManager::Categories::MissedCall:
+      return Symbols::phoneSlash;
+    case Pinetime::Controllers::NotificationManager::Categories::Sms:
+      return Symbols::smsBubble;
+    case Pinetime::Controllers::NotificationManager::Categories::VoiceMail:
+      return Symbols::voicemail;
+    case Pinetime::Controllers::NotificationManager::Categories::Schedule:
+      return Symbols::clock;
+    case Pinetime::Controllers::NotificationManager::Categories::HighProriotyAlert:
+      return Symbols::warningTriangle;
+    case Pinetime::Controllers::NotificationManager::Categories::InstantMessage:
+      return Symbols::messageBubble;
+    case Pinetime::Controllers::NotificationManager::Categories::Unknown:
     default:
-      return Symbols::info;
+      return Symbols::questionCircle;
   }  
 }
