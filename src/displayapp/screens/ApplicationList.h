280b3a4e6147f82957a2e7d12f87cf92045ec578
Yusuf Ebrahim <yusufmte@gmail.com>
2023-04-07 15:24:29 -0400

    update number of screens

--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -37,7 +37,7 @@ namespace Pinetime {
         static constexpr int appsPerScreen = 6;
 
         // Increment this when more space is needed
-        static constexpr int nScreens = 3;
+        static constexpr int nScreens = 2;
 
         static constexpr std::array<Tile::Applications, appsPerScreen * nScreens> applications {{
           {Symbols::stopWatch, Apps::StopWatch},
@@ -52,8 +52,8 @@ namespace Pinetime {
           {"2", Apps::Twos},
           {Symbols::drum, Apps::Metronome},
           {Symbols::map, Apps::Navigation},
-
           {Symbols::dice, Apps::Dice},
+
           // {"M", Apps::Motion},
         }};
         ScreenList<nScreens> screens;
