cfe21103ea197f98555d5002d389d0c24e7b5544
Finlay Davidson <finlay.davidson@coderclass.nl>
2023-06-25 15:59:34 +0200

    motioncontroller: Add functions for analysis
    These are functions for converting acceleration due to gravity to angles
    in degrees, and some statistical analysis including the mean and
    variance.

--- src/components/motion/MotionController.cpp
+++ src/components/motion/MotionController.cpp
@@ -2,7 +2,38 @@
 
 #include <task.h>
 
+#include "utility/Math.h"
+
 using namespace Pinetime::Controllers;
+
+namespace {
+  constexpr inline int32_t Clamp(int32_t val, int32_t min, int32_t max) {
+    return val < min ? min : (val > max ? max : val);
+  }
+
+  // only returns meaningful values if inputs are acceleration due to gravity
+  int16_t DegreesRolled(int16_t y, int16_t z, int16_t prevY, int16_t prevZ) {
+    int16_t prevYAngle = Pinetime::Utility::Asin(Clamp(prevY * 32, -32767, 32767));
+    int16_t yAngle = Pinetime::Utility::Asin(Clamp(y * 32, -32767, 32767));
+
+    if (z < 0 && prevZ < 0) {
+      return yAngle - prevYAngle;
+    }
+    if (prevZ < 0) {
+      if (y < 0) {
+        return -prevYAngle - yAngle - 180;
+      }
+      return -prevYAngle - yAngle + 180;
+    }
+    if (z < 0) {
+      if (y < 0) {
+        return prevYAngle + yAngle + 180;
+      }
+      return prevYAngle + yAngle - 180;
+    }
+    return prevYAngle - yAngle;
+  }
+}
 
 void MotionController::Update(int16_t x, int16_t y, int16_t z, uint32_t nbSteps) {
   if (this->nbSteps != nbSteps && service != nullptr) {
@@ -23,11 +54,37 @@ void MotionController::Update(int16_t x, int16_t y, int16_t z, uint32_t nbSteps)
   zHistory++;
   zHistory[0] = z;
 
+  stats = GetAccelStats();
+
   int32_t deltaSteps = nbSteps - this->nbSteps;
   if (deltaSteps > 0) {
     currentTripSteps += deltaSteps;
   }
   this->nbSteps = nbSteps;
+}
+
+MotionController::AccelStats MotionController::GetAccelStats() const {
+  AccelStats stats;
+
+  for (uint8_t i = 0; i < AccelStats::numHistory; i++) {
+    stats.yMean += yHistory[histSize - i];
+    stats.zMean += zHistory[histSize - i];
+    stats.prevYMean += yHistory[1 + i];
+    stats.prevZMean += zHistory[1 + i];
+  }
+  stats.yMean /= AccelStats::numHistory;
+  stats.zMean /= AccelStats::numHistory;
+  stats.prevYMean /= AccelStats::numHistory;
+  stats.prevZMean /= AccelStats::numHistory;
+
+  for (uint8_t i = 0; i < AccelStats::numHistory; i++) {
+    stats.yVariance += (yHistory[histSize - i] - stats.yMean) * (yHistory[histSize - i] - stats.yMean);
+    stats.zVariance += (zHistory[histSize - i] - stats.zMean) * (zHistory[histSize - i] - stats.zMean);
+  }
+  stats.yVariance /= AccelStats::numHistory;
+  stats.zVariance /= AccelStats::numHistory;
+
+  return stats;
 }
 
 bool MotionController::ShouldRaiseWake(bool isSleeping) {
