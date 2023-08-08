a924b69f567427ba924521fa2c27b7b948057063
Fuzzy Express <127012288+FuzzyExpress@users.noreply.github.com>
2023-08-08 15:56:22 -0500

    adding auto to other?

--- src/displayapp/screens/WatchFaceCasioStyleG7710.cpp
+++ src/displayapp/screens/WatchFaceCasioStyleG7710.cpp
@@ -222,6 +222,10 @@ void WatchFaceCasioStyleG7710::Refresh() {
   if (powerPresent.IsUpdated()) {
     lv_label_set_text_static(batteryPlug, BatteryIcon::GetPlugIcon(powerPresent.Get()));
 
+    auto batteryPercent = batteryPercentRemaining.Get();
+    batteryIcon.SetBatteryPercentage(batteryPercent);
+    lv_label_set_text_fmt(label_battery_value, "%d%%", batteryPercent);
+
     // Use the ColorRamp function from the batteryIcon object to get the desired color
     lv_color_t color_text = lv_color_hsv_to_rgb(batteryIcon.ColorRamp(batteryPercent), ( BatteryIcon::GetPlugIcon(powerPresent.Get()) == Symbols::plug ? 50 : 100 ), 100);
     
