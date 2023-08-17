a965f6b74461df41c961b01c945143a72ed76580
Fuzzy Express <evandsanders@gmail.com>
2023-08-16 20:50:30 -0500

    working?

--- src/displayapp/screens/Classes.cpp
+++ src/displayapp/screens/Classes.cpp
@@ -76,6 +76,8 @@ std::string Classes::timeUntilNextClass(std::unordered_map<std::string, int>& nu
 Classes::Classes(Pinetime::Controllers::DateTime& dateTimeController)
     : dateTimeController(dateTimeController) {
 
+      int OffSet = 3;
+
   label = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(label, "Next Class:", 0, 0, 0);
   lv_label_set_align(label, LV_LABEL_ALIGN_CENTER);
@@ -85,31 +87,31 @@ Classes::Classes(Pinetime::Controllers::DateTime& dateTimeController)
   next_class_name = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(next_class_name, "Loading Next Class", 0, 0, 0);
   lv_label_set_align(next_class_name, LV_LABEL_ALIGN_LEFT);
-  lv_obj_align(next_class_name, label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
+  lv_obj_align(next_class_name, label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, OffSet);
   lv_label_set_recolor(next_class_name, true);
 
   next_class_loc = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(next_class_loc, "Loading Class Loc", 0, 0, 0);
   lv_label_set_align(next_class_loc, LV_LABEL_ALIGN_LEFT);
-  lv_obj_align(next_class_loc, next_class_name, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
+  lv_obj_align(next_class_loc, next_class_name, LV_ALIGN_OUT_BOTTOM_LEFT, 0, OffSet);
   lv_label_set_recolor(next_class_loc, true);
 
   next_class_start = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(next_class_start, "Loading Class Start", 0, 0, 0);
   lv_label_set_align(next_class_start, LV_LABEL_ALIGN_LEFT);
-  lv_obj_align(next_class_start, next_class_loc, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
+  lv_obj_align(next_class_start, next_class_loc, LV_ALIGN_OUT_BOTTOM_LEFT, 0, OffSet);
   lv_label_set_recolor(next_class_start, true);
 
   next_class_end = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(next_class_end, "Loading Class End", 0, 0, 0);
   lv_label_set_align(next_class_end, LV_LABEL_ALIGN_LEFT);
-  lv_obj_align(next_class_end, next_class_start, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
+  lv_obj_align(next_class_end, next_class_start, LV_ALIGN_OUT_BOTTOM_LEFT, 0, OffSet);
   lv_label_set_recolor(next_class_end, true);
 
   next_class_dif = lv_label_create(lv_scr_act(), nullptr);
   lv_label_set_text_fmt(next_class_dif, "Loading Class End", 0, 0, 0);
   lv_label_set_align(next_class_dif, LV_LABEL_ALIGN_LEFT);
-  lv_obj_align(next_class_dif, next_class_end, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 5);
+  lv_obj_align(next_class_dif, next_class_end, LV_ALIGN_OUT_BOTTOM_LEFT, 0, OffSet);
   lv_label_set_recolor(next_class_dif, true);
 
   num_list["MWF"] = 1;
@@ -167,6 +169,6 @@ void Classes::Refresh() {
   lv_label_set_text_fmt(next_class_loc,   "%s",                           loc_list[nextClassNumber].c_str());
   lv_label_set_text_fmt(next_class_start, "Start: %s",                  start_list[nextClassNumber].c_str());
   lv_label_set_text_fmt(next_class_end,   "End  : %s",                    end_list[nextClassNumber].c_str());
-  lv_label_set_text_fmt(next_class_dif,   "In   : %s\nDay  : %s", result, "W");
+  lv_label_set_text_fmt(next_class_dif,   "In   : %s\nDay  : %d", result, nextClassNumber);
 
 }
