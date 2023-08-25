04ed068ff958f0107544bdd63442705e2930c85f
Patric Gruber <me@patric-gruber.at>
2023-08-25 02:10:45 +0200

    add settings screen to choose heartrate measurement background

--- src/heartratetask/HeartRateTask.h
+++ src/heartratetask/HeartRateTask.h
@@ -7,9 +7,6 @@
 
 #define DURATION_UNTIL_BACKGROUND_MEASURMENT_IS_STOPPED 30 * 1000 // 30 seconds assuming 1 Hz
 
-#define DURATION_BETWEEN_BACKGROUND_MEASUREMENTS        5 * 60 * 1000 // 5 minutes assuming 1 Hz
-#define DURATION_UNTIL_BACKGROUND_MEASURMENT_IS_STOPPED 30 * 1000     // 30 seconds assuming 1 Hz
-
 namespace Pinetime {
   namespace Drivers {
     class Hrs3300;
