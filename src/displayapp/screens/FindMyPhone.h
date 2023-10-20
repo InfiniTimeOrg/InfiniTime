286c5a4776c48a385ea52341fd023defae6b08ea
Jozef Mlich <jmlich83@gmail.com>
2023-10-20 16:13:40 +0200

    update screen layout

--- src/displayapp/screens/FindMyPhone.h
+++ src/displayapp/screens/FindMyPhone.h
@@ -5,6 +5,8 @@
 #include "displayapp/screens/Screen.h"
 #include "Symbols.h"
 #include "systemtask/SystemTask.h"
+#include "components/ble/ImmediateAlertService.h"
+
 #include <lvgl/src/lv_core/lv_style.h>
 #include <lvgl/src/lv_core/lv_obj.h>
 
@@ -22,17 +24,24 @@ namespace Pinetime {
         FindMyPhone(Pinetime::Controllers::ImmediateAlertService& immediateAlertService);
         ~FindMyPhone() override;
 
-        void OnStartStopEvent(lv_event_t event);
+        void OnImmediateAlertEvent(lv_obj_t* obj, lv_event_t event);
 
       private:
         Pinetime::Controllers::ImmediateAlertService& immediateAlertService;
 
-        void UpdateStartStopButton(bool isRunning);
-        lv_obj_t* label_title;
-        lv_obj_t* btn_startStop;
-        lv_obj_t* label_startStop;
+        void UpdateImmediateAlerts();
 
-        bool isFindMyPhoneRunning;
+        lv_obj_t* container;
+        lv_obj_t* label_title;
+        lv_obj_t* bt_none;
+        lv_obj_t* bt_high;
+        lv_obj_t* bt_mild;
+        lv_obj_t* label_none;
+        lv_obj_t* label_high;
+        lv_obj_t* label_mild;
+
+
+        Pinetime::Controllers::ImmediateAlertService::Levels last_level;
       };
     }
 
