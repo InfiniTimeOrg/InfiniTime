d79766bccd81b8b1ccc7f71b5aad9de3fc436193
Jean-François Milants <jf@codingfield.com>
2023-12-10 18:12:40 +0100

    Allow zero app to be selected at build time
    Fix 'nScreens' calculation in ApplicationList so that we can build the project with zero user app selected.

--- src/displayapp/screens/ApplicationList.h
+++ src/displayapp/screens/ApplicationList.h
@@ -38,7 +38,7 @@ namespace Pinetime {
 
         static constexpr int appsPerScreen = 6;
 
-        static constexpr int nScreens = (UserAppTypes::Count - 1) / appsPerScreen + 1;
+        static constexpr int nScreens = UserAppTypes::Count > 0 ? (UserAppTypes::Count - 1) / appsPerScreen + 1 : 1;
 
         ScreenList<nScreens> screens;
       };
