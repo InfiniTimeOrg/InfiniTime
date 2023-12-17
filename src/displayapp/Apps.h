810fc3d89abcb293683530b6263939af19e4a211
Jean-François Milants <jf@codingfield.com>
2023-12-17 17:35:36 +0100

    Watch face selection at build time
    Do not assign specific values to the fields of the enum WatchFace.

--- src/displayapp/Apps.h
+++ src/displayapp/Apps.h
@@ -44,12 +44,12 @@ namespace Pinetime {
     };
 
     enum class WatchFace : uint8_t {
-      Digital = 0,
-      Analog = 1,
-      PineTimeStyle = 2,
-      Terminal = 3,
-      Infineat = 4,
-      CasioStyleG7710 = 5,
+      Digital,
+      Analog,
+      PineTimeStyle,
+      Terminal,
+      Infineat,
+      CasioStyleG7710,
     };
 
     template <Apps>
