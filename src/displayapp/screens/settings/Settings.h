f13f0e3f9de815fcbc67031cd2bf0fc0ae21bbb9
Victor Kareh <vkareh@redhat.com>
2024-01-10 15:08:25 -0500

    settings: Add global widget selection
    Instead of each watch face implementing their own settings for which
    widgets to display, we can have a global selection of widgets. All
    watch faces can then determine whether it is enabled and so display it
    in whichever way makes sense for that face.
    
    Current widgets supported are heart rate, step counter, and weather.

--- src/displayapp/screens/settings/Settings.h
+++ src/displayapp/screens/settings/Settings.h
@@ -40,13 +40,14 @@ namespace Pinetime {
           {Symbols::shoe, "Steps", Apps::SettingSteps},
           {Symbols::clock, "Date&Time", Apps::SettingSetDateTime},
           {Symbols::cloudSunRain, "Weather", Apps::SettingWeatherFormat},
-          {Symbols::batteryHalf, "Battery", Apps::BatteryInfo},
+          {Symbols::wrench, "Widgets", Apps::SettingWidgets},
 
+          {Symbols::batteryHalf, "Battery", Apps::BatteryInfo},
           {Symbols::clock, "Chimes", Apps::SettingChimes},
           {Symbols::tachometer, "Shake Calib.", Apps::SettingShakeThreshold},
-          {Symbols::check, "Firmware", Apps::FirmwareValidation},
           {Symbols::bluetooth, "Bluetooth", Apps::SettingBluetooth},
 
+          {Symbols::check, "Firmware", Apps::FirmwareValidation},
           {Symbols::list, "About", Apps::SysInfo},
 
           // {Symbols::none, "None", Apps::None},
