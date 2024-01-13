034d83fe6baf1ab3875a34f8cee387e24410a824
Lennart Jahn <lennart@jahn42.de>
2024-01-13 14:05:04 +0100

    Split declaration and implementation of GetSymbol

--- src/displayapp/screens/WeatherSymbols.h
+++ src/displayapp/screens/WeatherSymbols.h
@@ -6,40 +6,7 @@ namespace Pinetime {
   namespace Applications {
     namespace Screens {
       namespace Symbols {
-        const char* GetSymbol(const Pinetime::Controllers::SimpleWeatherService::Icons icon) {
-          switch (icon) {
-            case Pinetime::Controllers::SimpleWeatherService::Icons::Sun:
-              return Symbols::sun;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::CloudsSun:
-              return Symbols::cloudSun;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::Clouds:
-              return Symbols::cloud;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::BrokenClouds:
-              return Symbols::cloudMeatball;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::Thunderstorm:
-              return Symbols::bolt;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::Snow:
-              return Symbols::snowflake;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::CloudShowerHeavy:
-              return Symbols::cloudShowersHeavy;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::CloudSunRain:
-              return Symbols::cloudSunRain;
-              break;
-            case Pinetime::Controllers::SimpleWeatherService::Icons::Smog:
-              return Symbols::smog;
-              break;
-            default:
-              return Symbols::ban;
-              break;
-          }
-        }
+        const char* GetSymbol(const Pinetime::Controllers::SimpleWeatherService::Icons icon);
       }
     }
   }
