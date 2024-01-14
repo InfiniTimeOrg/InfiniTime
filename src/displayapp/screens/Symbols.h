3c4027ab3cf6231f7de64822db51929ab696a8e7
Yusuf Ebrahim <yusufmte@gmail.com>
2024-01-13 22:13:59 -0500

    removed vestigial chartLine symbol

--- src/displayapp/screens/Symbols.h
+++ src/displayapp/screens/Symbols.h
@@ -35,7 +35,6 @@ namespace Pinetime {
         static constexpr const char* lapsFlag = "\xEF\x80\xA4";
         static constexpr const char* drum = "\xEF\x95\xA9";
         static constexpr const char* dice = "\xEF\x94\xA2";
-        static constexpr const char* chartLine = "\xEF\x88\x81";
         static constexpr const char* eye = "\xEF\x81\xAE";
         static constexpr const char* home = "\xEF\x80\x95";
         static constexpr const char* sleep = "\xEE\xBD\x84";
