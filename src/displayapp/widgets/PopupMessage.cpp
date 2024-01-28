a812f6830ec9031c7447ae8bec41afc3128beac0
Tomas Groth <second@tgc.dk>
2024-01-28 21:38:53 +0100

    Replace the text in the popup with a lock icon.

--- src/displayapp/widgets/PopupMessage.cpp
+++ src/displayapp/widgets/PopupMessage.cpp
@@ -3,15 +3,16 @@
 
 using namespace Pinetime::Applications::Widgets;
 
-PopupMessage::PopupMessage(const char* msg) : message {msg} {
+PopupMessage::PopupMessage(const char* msg, int16_t h, int16_t w) : message{msg}, height{h}, width{w} {
 }
 
 void PopupMessage::Create() {
   btnPopup = lv_btn_create(lv_scr_act(), nullptr);
   btnPopup->user_data = this;
-  lv_obj_set_size(btnPopup, 200, 150);
+  lv_obj_set_size(btnPopup, height, width);
   lv_obj_align(btnPopup, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
   lv_obj_set_style_local_bg_opa(btnPopup, LV_BTN_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_60);
+  lv_obj_set_style_local_text_font(btnPopup, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, &lv_font_sys_80);
   lv_obj_t* lblMessage = lv_label_create(btnPopup, nullptr);
   lv_label_set_text_static(lblMessage, message);
   lv_obj_set_hidden(btnPopup, isHidden);
