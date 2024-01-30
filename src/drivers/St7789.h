86e7664e68c5ccb5e1c094e4f29fe7571c573d44
mark9064 <30447455+mark9064@users.noreply.github.com>
2024-01-30 23:29:16 +0000

    Apply display driver datasheet delays

--- src/drivers/St7789.h
+++ src/drivers/St7789.h
@@ -2,6 +2,10 @@
 #include <cstddef>
 #include <cstdint>
 
+#include <hal/nrf_gpio.h>
+#include <nrfx_log.h>
+#include "drivers/Spi.h"
+
 namespace Pinetime {
   namespace Drivers {
     class Spi;
@@ -29,10 +33,13 @@ namespace Pinetime {
       uint8_t pinDataCommand;
       uint8_t pinReset;
       uint8_t verticalScrollingStartAddress = 0;
+      bool sleepIn;
+      TickType_t lastSleepExit;
 
       void HardwareReset();
       void SoftwareReset();
       void SleepOut();
+      void EnsureSleepOutPostDelay();
       void SleepIn();
       void ColMod();
       void MemoryDataAccessControl();
