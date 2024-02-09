d7f2e15a236f93ba11cbc317ce49d7466ee1c5df
Victor Kareh <vkareh@redhat.com>
2024-02-09 13:19:44 -0500

    settings: Add setting for auto-opening apps
    Certain apps can be configured to auto-open based on component events:
    - battery: when charging the watch
    - music: when playing music from the paired device
    - navigation: when opening a maps app on the paired device

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -47,6 +47,7 @@
 #include "displayapp/screens/settings/SettingSteps.h"
 #include "displayapp/screens/settings/SettingSetDateTime.h"
 #include "displayapp/screens/settings/SettingChimes.h"
+#include "displayapp/screens/settings/SettingAutoOpen.h"
 #include "displayapp/screens/settings/SettingShakeThreshold.h"
 #include "displayapp/screens/settings/SettingBluetooth.h"
 
@@ -625,6 +626,9 @@ void DisplayApp::LoadScreen(Apps app, DisplayApp::FullRefreshDirections directio
       break;
     case Apps::SettingChimes:
       currentScreen = std::make_unique<Screens::SettingChimes>(settingsController);
+      break;
+    case Apps::SettingAutoOpen:
+      currentScreen = std::make_unique<Screens::SettingAutoOpen>(settingsController);
       break;
     case Apps::SettingShakeThreshold:
       currentScreen = std::make_unique<Screens::SettingShakeThreshold>(settingsController, motionController, *systemTask);
