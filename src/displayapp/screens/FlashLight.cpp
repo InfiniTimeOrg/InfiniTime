3b3701c1b5679a2fc3422e75aa7c07f149893f83
Jean-François Milants <jf@codingfield.com>
2024-03-14 21:20:41 +0100

    Restore brightness level when closing the Flashlight application
    The brightness level is stored when entering the app, and restored when closing it. Based on another fixed suggested by serosi.
    
    Co-authored-by: serosi <68392668+serosi@users.noreply.github.com>

--- src/displayapp/screens/FlashLight.cpp
+++ src/displayapp/screens/FlashLight.cpp
@@ -15,7 +15,7 @@ namespace {
 }
 
 FlashLight::FlashLight(System::SystemTask& systemTask, Controllers::BrightnessController& brightnessController)
-  : systemTask {systemTask}, brightnessController {brightnessController} {
+  : systemTask {systemTask}, brightnessController {brightnessController}, backupBrightnessLevel {brightnessController.Level()} {
 
   brightnessController.Set(Controllers::BrightnessController::Levels::Low);
 
@@ -52,6 +52,7 @@ FlashLight::FlashLight(System::SystemTask& systemTask, Controllers::BrightnessCo
 FlashLight::~FlashLight() {
   lv_obj_clean(lv_scr_act());
   lv_obj_set_style_local_bg_color(lv_scr_act(), LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  brightnessController.Set(backupBrightnessLevel);
   systemTask.PushMessage(Pinetime::System::Messages::EnableSleeping);
 }
 
