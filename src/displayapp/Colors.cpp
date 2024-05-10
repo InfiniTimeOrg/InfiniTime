6d4d2ac20e8ee690b57f4ff5c49e4600bdd68a40
Michael Adams <me@michdavidadams.com>
2024-05-10 17:45:21 -0400

    colors

--- src/displayapp/Colors.cpp
+++ src/displayapp/Colors.cpp
@@ -6,41 +6,41 @@ using namespace Pinetime::Controllers;
 lv_color_t Pinetime::Applications::Convert(Pinetime::Controllers::Settings::Colors color) {
   switch (color) {
     case Pinetime::Controllers::Settings::Colors::White:
-      return LV_COLOR_WHITE;
+      return LV_COLOR_MAKE(0xfb, 0xf1, 0xc7);
     case Pinetime::Controllers::Settings::Colors::Silver:
-      return LV_COLOR_SILVER;
+      return LV_COLOR_MAKE(0xa8, 0x99, 0x84);
     case Pinetime::Controllers::Settings::Colors::Gray:
-      return LV_COLOR_GRAY;
+      return LV_COLOR_MAKE(0x92, 0x83, 0x74);
     case Pinetime::Controllers::Settings::Colors::Black:
-      return LV_COLOR_BLACK;
+      return LV_COLOR_MAKE(0x1d, 0x20, 0x21);
     case Pinetime::Controllers::Settings::Colors::Red:
-      return LV_COLOR_RED;
+      return LV_COLOR_MAKE(0xfb, 0x49, 0x34);
     case Pinetime::Controllers::Settings::Colors::Maroon:
-      return LV_COLOR_MAKE(0xb0, 0x0, 0x0);
+      return LV_COLOR_MAKE(0xcc, 0x24, 0x1d);
     case Pinetime::Controllers::Settings::Colors::Yellow:
-      return LV_COLOR_YELLOW;
+      return LV_COLOR_MAKE(0xfa, 0xbd, 0x2f);
     case Pinetime::Controllers::Settings::Colors::Olive:
-      return LV_COLOR_MAKE(0xb0, 0xb0, 0x0);
+      return LV_COLOR_MAKE(0xd7, 0x99, 0x21);
     case Pinetime::Controllers::Settings::Colors::Lime:
-      return LV_COLOR_LIME;
+      return LV_COLOR_MAKE(0xb8, 0xbb, 0x26);
     case Pinetime::Controllers::Settings::Colors::Green:
-      return LV_COLOR_MAKE(0x0, 0xb0, 0x0);
+      return LV_COLOR_MAKE(0x98, 0x97, 0x1a);
     case Pinetime::Controllers::Settings::Colors::Cyan:
-      return LV_COLOR_CYAN;
+      return LV_COLOR_MAKE(0x8e, 0xc0, 0x7c);
     case Pinetime::Controllers::Settings::Colors::Teal:
-      return LV_COLOR_MAKE(0x0, 0xb0, 0xb0);
+      return LV_COLOR_MAKE(0x68, 0x9d, 0x6a);
     case Pinetime::Controllers::Settings::Colors::Blue:
-      return LV_COLOR_BLUE;
+      return LV_COLOR_MAKE(0x83, 0xa5, 0x98);
     case Pinetime::Controllers::Settings::Colors::Navy:
-      return LV_COLOR_MAKE(0x0, 0x0, 0xb0);
+      return LV_COLOR_MAKE(0x45, 0x85, 0x88);
     case Pinetime::Controllers::Settings::Colors::Magenta:
-      return LV_COLOR_MAGENTA;
+      return LV_COLOR_MAKE(0xd3, 0x86, 0x9b);
     case Pinetime::Controllers::Settings::Colors::Purple:
-      return LV_COLOR_MAKE(0xb0, 0x0, 0xb0);
+      return LV_COLOR_MAKE(0xb1, 0x62, 0x86);
     case Pinetime::Controllers::Settings::Colors::Orange:
-      return LV_COLOR_ORANGE;
+      return LV_COLOR_MAKE(0xfe, 0x80, 0x19);
     case Pinetime::Controllers::Settings::Colors::Pink:
-      return LV_COLOR_MAKE(0xFF, 0xAE, 0xC9);
+      return LV_COLOR_MAKE(0xd6, 0x5d, 0x0e);
     default:
       return LV_COLOR_WHITE;
   }
--- src/displayapp/InfiniTimeTheme.h
+++ src/displayapp/InfiniTimeTheme.h
@@ -3,16 +3,16 @@
 #include <lvgl/lvgl.h>
 
 namespace Colors {
-  static constexpr lv_color_t deepOrange = LV_COLOR_MAKE(0xff, 0x40, 0x0);
-  static constexpr lv_color_t orange = LV_COLOR_MAKE(0xff, 0xb0, 0x0);
-  static constexpr lv_color_t green = LV_COLOR_MAKE(0x0, 0xb0, 0x0);
-  static constexpr lv_color_t blue = LV_COLOR_MAKE(0x0, 0x50, 0xff);
-  static constexpr lv_color_t lightGray = LV_COLOR_MAKE(0xb0, 0xb0, 0xb0);
+  static constexpr lv_color_t deepOrange = LV_COLOR_MAKE(0xd6, 0x5d, 0x0e);
+  static constexpr lv_color_t orange = LV_COLOR_MAKE(0xfe, 0x80, 0x19);
+  static constexpr lv_color_t green = LV_COLOR_MAKE(0x98, 0x97, 0x1a);
+  static constexpr lv_color_t blue = LV_COLOR_MAKE(0x45, 0x85, 0x88);
+  static constexpr lv_color_t lightGray = LV_COLOR_MAKE(0xa8, 0x99, 0x84);
 
-  static constexpr lv_color_t bg = LV_COLOR_MAKE(0x5d, 0x69, 0x7e);
-  static constexpr lv_color_t bgAlt = LV_COLOR_MAKE(0x38, 0x38, 0x38);
-  static constexpr lv_color_t bgDark = LV_COLOR_MAKE(0x18, 0x18, 0x18);
-  static constexpr lv_color_t highlight = green;
+  static constexpr lv_color_t bg = LV_COLOR_MAKE(0x50, 0x49, 0x45);
+  static constexpr lv_color_t bgAlt = LV_COLOR_MAKE(0x3c, 0x38, 0x36);
+  static constexpr lv_color_t bgDark = LV_COLOR_MAKE(0x28, 0x28, 0x28);
+  static constexpr lv_color_t highlight = blue;
 };
 
 /**
