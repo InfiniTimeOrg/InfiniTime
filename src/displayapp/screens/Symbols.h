ee99e554f0bf54d76190b7b06727baa8a3f1aaaf
ecarlett <eve.carletti@epfl.ch>
2024-05-29 13:15:28 +0200

    copy :( files from gitlab repo
     interactive rebase in progress; onto b7a353cf
     Last command done (1 command done):
        pick 504f5774 copy :( files from gitlab repo
     Next commands to do (2 remaining commands):
        pick 9d04c32f recovering manually changes to original Infineat
        pick 70f9025f now I have the Meow Watchface AND the Infineat Watchface with alarm display
     You are currently rebasing branch 'my-custom-infinitime' on 'b7a353cf'.
    
     Changes to be committed:
    	modified:   src/CMakeLists.txt
    	modified:   src/displayapp/apps/Apps.h.in
    	modified:   src/displayapp/apps/CMakeLists.txt
    	modified:   src/displayapp/screens/Symbols.h

--- src/displayapp/screens/Symbols.h
+++ src/displayapp/screens/Symbols.h
@@ -14,7 +14,7 @@ namespace Pinetime {
         static constexpr const char* clock = "\xEF\x80\x97";
         static constexpr const char* bell = "\xEF\x83\xB3";
         static constexpr const char* notbell = "\xEF\x87\xB6";
-        static constexpr const char* info = "\xEF\x84\xA9";
+	static constexpr const char* info = "\xEF\x84\xA9";
         static constexpr const char* list = "\xEF\x80\xBA";
         static constexpr const char* sun = "\xEF\x86\x85";
         static constexpr const char* check = "\xEF\x95\xA0";
@@ -41,14 +41,11 @@ namespace Pinetime {
         static constexpr const char* eye = "\xEF\x81\xAE";
         static constexpr const char* home = "\xEF\x80\x95";
         static constexpr const char* sleep = "\xEE\xBD\x84";
-<<<<<<< HEAD
         static constexpr const char* calendar = "\xEF\x81\xB3";
-=======
         static constexpr const char* bird = "\xEF\x92\xBA";
         static constexpr const char* zzz = "\xEF\x88\xB6";
->>>>>>> d7de641b (added symbols for alarm states, added alarm state and hour to watchface meow, changed steps to paw. Need to align everythign nicely and find where to change release number)
-
-        // fontawesome_weathericons.c
+        
+	// fontawesome_weathericons.c
         // static constexpr const char* sun = "\xEF\x86\x85";
         static constexpr const char* cloudSun = "\xEF\x9B\x84";
         static constexpr const char* cloudSunRain = "\xEF\x9D\x83";
