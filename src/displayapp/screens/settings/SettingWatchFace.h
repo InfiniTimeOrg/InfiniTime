25e81b159b2b76b6d6340affc8ebe2398733dfc2
ecarlett <eve.carletti@epfl.ch>
2024-05-29 15:54:56 +0200

    now there is my watchface in pink and stuff adn the Infineat watchface with alarm status but the icons are not the same as on branch alarm-on-infineat

--- src/displayapp/screens/settings/SettingWatchFace.h
+++ src/displayapp/screens/settings/SettingWatchFace.h
@@ -10,7 +10,7 @@
 #include "displayapp/screens/Symbols.h"
 #include "displayapp/screens/CheckboxList.h"
 #include "displayapp/screens/WatchFaceInfineat.h"
-include "displayapp/screens/WatchFaceMeow.h"
+#include "displayapp/screens/WatchFaceMeow.h"
 #include "displayapp/screens/WatchFaceCasioStyleG7710.h"
 
 
