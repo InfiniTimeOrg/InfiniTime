1ace7858965e3c2176d6258b4fd0144b75ea469b
Reinhold Gschweicher <pyro4hell@gmail.com>
2024-06-25 22:54:56 +0200

    lv_font_conv: finish all lvgl sections, simple cmaps

--- src/resources/lv_font_conv.py
+++ src/resources/lv_font_conv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from dataclasses import dataclass, field
+import itertools
 import math
 import pathlib
 import sys
@@ -67,7 +68,9 @@ class FontAction(argparse.Action):
 @dataclass
 class LVGlyph:
     character: str
+    glyph_id: int
     bitmap: bytearray
+    bitmap_index: int
     advance_width: int
     bbox_width: int
     bbox_height: int
@@ -81,6 +84,7 @@ class LVFont:
     bpp: int = 1
     opts: str = ""
     glyphs: list[LVGlyph] = field(default_factory=list)
+    cmaps: list[list[LVGlyph]] = field(default_factory=list)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -193,10 +197,14 @@ def main():
         #print(tt.keys())
         # https://stackoverflow.com/questions/70368410/how-to-render-a-ttf-glyf-to-a-image-with-fonttools
         # https://pillow.readthedocs.io/en/stable/reference/ImageFont.html
+        # https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html
         font = ImageFont.truetype(font_arg.font, args.size)
         ascent, descent = font.getmetrics()
         print(f"- ascent/descent: {ascent}, {descent}")
-        text = font_arg.symbols
+        # symbols are sorted by their ASCII index, each glyp only once
+        text = "".join(sorted(set(font_arg.symbols), key=ord))
+
+        bitmap_index = 0
         for c in text:
             (width, baseline), (offset_x, offset_y) = font.font.getsize(c)
             (left, top, right, bottom) = font.getbbox(c)
@@ -206,13 +214,16 @@ def main():
             print(f"- '{c}': length: {length}")
             lv_glyph = LVGlyph(
                 character=c,
+                glyph_id=len(lv_font.glyphs) + 1,
                 bitmap=bytearray(b" "),
+                bitmap_index=bitmap_index,
                 advance_width=length,
-                bbox_width=right - left,
-                bbox_height=top - bottom,
+                bbox_width=abs(right - left),
+                bbox_height=abs(top - bottom),
                 offset_x=offset_x,
                 offset_y=offset_y,
             )
+            bitmap_index += len(lv_glyph.bitmap)
             lv_font.glyphs.append(lv_glyph)
 
         length = math.ceil(font.getlength(text))
@@ -220,6 +231,11 @@ def main():
         draw = ImageDraw.Draw(image)
         draw.text((0,-descent), text, font=font)
         image.save(f"out_{idx}.png")
+
+    # cmaps of consecutive glyphs
+    for k, g in itertools.groupby(enumerate(lv_font.glyphs), lambda x: x[0]-ord(x[1].character)):
+        lv_font.cmaps.append(list(map(lambda x: x[1], g)))
+
     if args.format == "lvgl":
         with open(args.output, "w", encoding="utf-8") as f:
             f.write(to_lvgl(lv_font))
@@ -255,7 +271,108 @@ def to_lvgl(font: LVFont):
  *----------------*/
 
 /*Store the image of the glyphs*/
-static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {{
+static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {{"""
+    for i, g in enumerate(font.glyphs):
+        bytes_str = ", ".join([f"0x{b:x}" for b in g.bitmap])
+        if g.character.isprintable():
+            char_str = g.character if g.character != '"' else '\\"'
+        else:
+            char_str = f"\\u{ord(g.character):04X}"
+        out += f"""
+    /* U+{ord(g.character):04X} "{char_str}" */
+    {bytes_str}{",\n" if i+1 < len(font.glyphs) else ""}"""
+    out += f"""
+}};
+
+
+/*---------------------
+ *  GLYPH DESCRIPTION
+ *--------------------*/
+
+static const lv_font_fmt_txt_glyph_dsc_t glyph_dsc[] = {{
+    {{.bitmap_index = 0, .adv_w = 0, .box_w = 0, .box_h = 0, .ofs_x = 0, .ofs_y = 0}} /* id = 0 reserved */,"""
+    for i, g in enumerate(font.glyphs):
+        bytes_str = ", ".join([f"0x{b:x}" for b in g.bitmap])
+        out += f"""
+    {{.bitmap_index = {g.bitmap_index}, .adv_w = {g.advance_width}, .box_w = {g.bbox_width}, .box_h = {g.bbox_height}, .ofs_x = {g.offset_x}, .ofs_y = {g.offset_y}}}{"," if i+1 < len(font.glyphs) else ""}"""
+    out += f"""
+}};
+
+/*---------------------
+ *  CHARACTER MAPPING
+ *--------------------*/
+
+// TODO: figure out when to do a unicode_list_X[]
+// TODO: figure out when to create SPARSE_TINY or FORMAT0_TINY
+
+/*Collect the unicode lists and glyph_id offsets*/
+static const lv_font_fmt_txt_cmap_t cmaps[] =
+{{"""
+    for i, cmap in enumerate(font.cmaps):
+        out += f"""
+    {{
+        .range_start = {ord(cmap[0].character)}, .range_length = {len(cmap)}, .glyph_id_start = {cmap[0].glyph_id},
+        .unicode_list = NULL, .glyph_id_ofs_list = NULL, .list_length = 0, .type = LV_FONT_FMT_TXT_CMAP_FORMAT0_TINY
+    }}{"," if i+1 < len(font.cmaps) else ""}"""
+    out += f"""
+}};
+
+
+
+/*--------------------
+ *  ALL CUSTOM DATA
+ *--------------------*/
+
+#if LVGL_VERSION_MAJOR >= 8
+/*Store all the custom data of the font*/
+static  lv_font_fmt_txt_glyph_cache_t cache;
+static const lv_font_fmt_txt_dsc_t font_dsc = {{
+#else
+static lv_font_fmt_txt_dsc_t font_dsc = {{
+#endif
+    .glyph_bitmap = glyph_bitmap,
+    .glyph_dsc = glyph_dsc,
+    .cmaps = cmaps,
+    .kern_dsc = NULL,
+    .kern_scale = 0,
+    .cmap_num = {len(font.cmaps)},
+    .bpp = {font.bpp},
+    .kern_classes = 0,
+    .bitmap_format = 0,
+#if LVGL_VERSION_MAJOR >= 8
+    .cache = &cache
+#endif
+}};
+
+
+/*-----------------
+ *  PUBLIC FONT
+ *----------------*/
+
+/*Initialize a public general font descriptor*/
+#if LVGL_VERSION_MAJOR >= 8
+const lv_font_t jetbrains_mono_bold_20_js = {{
+#else
+lv_font_t jetbrains_mono_bold_20_js = {{
+#endif
+    .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
+    .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
+    .line_height = {max([g.bbox_height for g in font.glyphs])},          /*The maximum line height required by the font*/
+    .base_line = {min([g.offset_y for g in font.glyphs])},             /*Baseline measured from the bottom of the line*/
+#if !(LVGL_VERSION_MAJOR == 6 && LVGL_VERSION_MINOR == 0)
+    .subpx = LV_FONT_SUBPX_NONE,
+#endif
+#if LV_VERSION_CHECK(7, 4, 0) || LVGL_VERSION_MAJOR >= 8
+    .underline_position = XXX,
+    .underline_thickness = XXX,
+#endif
+    .dsc = &font_dsc           /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
+}};
+
+
+
+#endif /*#if JETBRAINS_MONO_BOLD_20_JS*/
+
 """
 
     return out
