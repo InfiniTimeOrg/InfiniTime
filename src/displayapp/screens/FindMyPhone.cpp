1fe73d7704f663d3226404fa35b5c37c7f0ab8e5
Vyacheslav Chigrin <vyacheslav.chigrin@izba.dev>
2024-11-28 00:42:03 +0300

    Update UI. Remove "Mild" level button.

--- src/displayapp/screens/FindMyPhone.cpp
+++ src/displayapp/screens/FindMyPhone.cpp
@@ -10,6 +10,8 @@ namespace {
   static constexpr char defaultLabelText[] = "Find my phone";
   static constexpr char alertSentLabelText[] = "Alert sent";
   static constexpr char noConnectionLabelText[] = "No connection";
+  static constexpr char noneLabelText[] = "Stop";
+  static constexpr char highLabelText[] = "Ring";
   static constexpr auto restoreLabelTimeoutTicks = pdMS_TO_TICKS(2 * 1000);
 
   void btnImmediateAlertEventHandler(lv_obj_t* obj, lv_event_t event) {
@@ -44,28 +46,19 @@ FindMyPhone::FindMyPhone(Pinetime::Controllers::ImmediateAlertClient& immediateA
   bt_none = lv_btn_create(container, nullptr);
   bt_none->user_data = this;
   lv_obj_set_event_cb(bt_none, btnImmediateAlertEventHandler);
-  lv_obj_set_size(bt_none, 76, 76);
+  lv_obj_set_size(bt_none, 114, 76);
   lv_obj_align(bt_none, nullptr, LV_ALIGN_IN_BOTTOM_LEFT, 0, 0);
   label_none = lv_label_create(bt_none, nullptr);
-  lv_label_set_text_static(label_none, "None");
+  lv_label_set_text_static(label_none, noneLabelText);
   lv_obj_set_style_local_bg_color(bt_none, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Colors::lightGray);
-
-  bt_mild = lv_btn_create(container, nullptr);
-  bt_mild->user_data = this;
-  lv_obj_set_event_cb(bt_mild, btnImmediateAlertEventHandler);
-  lv_obj_set_size(bt_mild, 76, 76);
-  lv_obj_align(bt_mild, nullptr, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
-  label_mild = lv_label_create(bt_mild, nullptr);
-  lv_label_set_text_static(label_mild, "Mild");
-  lv_obj_set_style_local_bg_color(bt_mild, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Colors::highlight);
 
   bt_high = lv_btn_create(container, nullptr);
   bt_high->user_data = this;
   lv_obj_set_event_cb(bt_high, btnImmediateAlertEventHandler);
-  lv_obj_set_size(bt_high, 76, 76);
+  lv_obj_set_size(bt_high, 114, 76);
   lv_obj_align(bt_high, nullptr, LV_ALIGN_IN_BOTTOM_RIGHT, 0, 0);
   label_high = lv_label_create(bt_high, nullptr);
-  lv_label_set_text_static(label_high, "High");
+  lv_label_set_text_static(label_high, highLabelText);
   lv_obj_set_style_local_bg_color(bt_high, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
 }
 
@@ -77,8 +70,6 @@ void FindMyPhone::OnImmediateAlertEvent(lv_obj_t* obj, lv_event_t event) {
   if (event == LV_EVENT_CLICKED) {
     if (obj == bt_none) {
       last_level = Pinetime::Controllers::ImmediateAlertClient::Levels::NoAlert;
-    } else if (obj == bt_mild) {
-      last_level = Pinetime::Controllers::ImmediateAlertClient::Levels::MildAlert;
     } else if (obj == bt_high) {
       last_level = Pinetime::Controllers::ImmediateAlertClient::Levels::HighAlert;
     }
@@ -91,11 +82,12 @@ void FindMyPhone::UpdateImmediateAlerts() {
     case Pinetime::Controllers::ImmediateAlertClient::Levels::NoAlert:
       lv_obj_set_style_local_text_color(label_title, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Colors::lightGray);
       break;
-    case Pinetime::Controllers::ImmediateAlertClient::Levels::MildAlert:
-      lv_obj_set_style_local_text_color(label_title, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, Colors::highlight);
-      break;
     case Pinetime::Controllers::ImmediateAlertClient::Levels::HighAlert:
       lv_obj_set_style_local_text_color(label_title, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
+      break;
+    case Pinetime::Controllers::ImmediateAlertClient::Levels::MildAlert:
+      // Not supported.
+      ASSERT(false);
       break;
   }
   if (immediateAlertClient.sendImmediateAlert(last_level)) {
