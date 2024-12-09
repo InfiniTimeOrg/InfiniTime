c70a9aac3f503d35e8e29c47690352e8e56a6f76
FintasticMan <finlay.neon.kid@gmail.com>
2024-12-10 00:11:13 +0100

    weather: Fix incorrect rounding for negative temperatures

--- src/components/ble/SimpleWeatherService.h
+++ src/components/ble/SimpleWeatherService.h
@@ -34,6 +34,7 @@
 #include "components/datetime/DateTimeController.h"
 #include <lvgl/lvgl.h>
 #include "displayapp/InfiniTimeTheme.h"
+#include "utility/Math.h"
 
 int WeatherCallback(uint16_t connHandle, uint16_t attrHandle, struct ble_gatt_access_ctxt* ctxt, void* arg);
 
@@ -77,11 +78,11 @@ namespace Pinetime {
         }
 
         [[nodiscard]] int16_t Celsius() const {
-          return (PreciseCelsius() + 50) / 100;
+          return Utility::RoundedDiv(PreciseCelsius(), static_cast<int16_t>(100));
         }
 
         [[nodiscard]] int16_t Fahrenheit() const {
-          return (PreciseFahrenheit() + 50) / 100;
+          return Utility::RoundedDiv(PreciseFahrenheit(), static_cast<int16_t>(100));
         }
 
         [[nodiscard]] lv_color_t Color() const {
