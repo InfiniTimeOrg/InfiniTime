d033f19e7fb39d2014d112175e1dcdf1bc0604ff
Titus Kendzorra <titus@kendzorra.de>
2025-01-21 14:35:38 +0100

    added a space after a comment //

--- src/displayapp/screens/Timer.cpp
+++ src/displayapp/screens/Timer.cpp
@@ -60,7 +60,7 @@ Timer::Timer(Controllers::Timer& timerController) : timer {timerController} {
   lv_obj_set_size(btnPlayPause, LV_HOR_RES, 50);
 
   txtPlayPause = lv_label_create(btnPlayPause, nullptr);
-  //lv_obj_align(txtPlayPause, btnPlayPause, LV_ALIGN_CENTER, 0, 0);
+  // lv_obj_align(txtPlayPause, btnPlayPause, LV_ALIGN_CENTER, 0, 0);
 
   if (timer.IsRunning()) {
     SetTimerRunning();
