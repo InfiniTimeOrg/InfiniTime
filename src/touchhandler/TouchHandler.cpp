59cee628b11b3930d78b1d3ee911cdeef2c78685
Reinhold Gschweicher <pyro4hell@gmail.com>
2025-05-11 21:38:56 +0200

    TouchHandler: restore old ProcessTouchInfo, remove WakeUp code

--- src/touchhandler/TouchHandler.cpp
+++ src/touchhandler/TouchHandler.cpp
@@ -33,9 +33,9 @@ Pinetime::Applications::TouchEvents TouchHandler::GestureGet() {
   return returnGesture;
 }
 
-TouchHandler::TouchProcessReply TouchHandler::ProcessTouchInfo(Drivers::Cst816S::TouchInfos info, bool buttonUnlocksOn) {
+bool TouchHandler::ProcessTouchInfo(Drivers::Cst816S::TouchInfos info) {
   if (!info.isValid) {
-    return TouchHandler::TouchProcessReply::NoAction;
+    return false;
   }
 
   // Only a single gesture per touch
@@ -61,20 +61,5 @@ TouchHandler::TouchProcessReply TouchHandler::ProcessTouchInfo(Drivers::Cst816S:
   }
 
   currentTouchPoint = {info.x, info.y, info.touching};
-
-  // if the watch was just woken by touch and button must be used to unlock, ignore the first touch event which
-  // is the touch event that woke the watch. Otherwise the lock-popup will be displayed
-  if (buttonUnlocksOn && ignoreNextTouchEvent) {
-    ignoreNextTouchEvent = false;
-    return TouchHandler::TouchProcessReply::NoAction;
-
-  } else if (!buttonUnlocksOn || wokenBy != WokenBy::WakeUpAction) {
-
-    // if we get to here TouchEvents is allowed and the "ButtonUnlocks" requirement can be overridden
-    wokenBy = WokenBy::Other;
-
-    return TouchHandler::TouchProcessReply::TouchEvent;
-  } else {
-    return TouchHandler::TouchProcessReply::IgnoreTouchPopup;
-  }
+  return true;
 }
