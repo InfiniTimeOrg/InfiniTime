f4aa76cb0e11c099bbb5414aaecbcf485c0be68d
Titus Kendzorra <titus@kendzorra.de>
2025-05-30 17:29:21 +0200

    correctly include new font

--- src/libs/lv_conf.h
+++ src/libs/lv_conf.h
@@ -419,7 +419,8 @@ typedef void* lv_indev_drv_user_data_t;            /*Type of user data in the in
                                LV_FONT_DECLARE(jetbrains_mono_76) \
                                LV_FONT_DECLARE(open_sans_light) \
                                LV_FONT_DECLARE(fontawesome_weathericons) \
-                               LV_FONT_DECLARE(lv_font_sys_48)
+                               LV_FONT_DECLARE(lv_font_sys_48) \
+                               LV_FONT_DECLARE(prime)
 
 /* Enable it if you have fonts with a lot of characters.
  * The limit depends on the font size, font face and bpp
