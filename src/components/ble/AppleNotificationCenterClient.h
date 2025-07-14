9c919d3ec5154d65317d3b8311711d53ae371f28
cyberneel <neelesh.chevuri25@gmail.com>
2025-07-13 22:17:11 -0500

    Added a session uuid cache to try to help with duplicate pings

--- src/components/ble/AppleNotificationCenterClient.h
+++ src/components/ble/AppleNotificationCenterClient.h
@@ -45,6 +45,12 @@ namespace Pinetime {
       static constexpr uint8_t maxSubtitleSize {15};
       static constexpr uint8_t maxMessageSize {120};
 
+      // Keep track of the ANCS UUIDs in array for this session to avoid re-pings for old notifications
+      // Keep track of max notifications UUIDs at most
+      static constexpr uint8_t maxNotifications {200};
+      // make this unordered map have a boolean attached to see if data has been received for this notification
+      std::unordered_map<uint32_t, bool> sessionNotificationUids {};
+
       // The Apple Notification Center Service UUID are from https://developer.apple.com/library/archive/documentation/CoreBluetooth/Reference/AppleNotificationCenterServiceSpecification/Specification/Specification.html
 
       // 7905F431-B5CE-4E99-A40F-4B1E122D00D0
