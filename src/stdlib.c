5907b1235bdbe75dd554dc5a27534d77d1f9a909
mark9064 <30447455+mark9064@users.noreply.github.com>
2026-08-08 23:45:00 +0100

    Implement required stubs instead of linking nosys

--- src/stdlib.c
+++ src/stdlib.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <FreeRTOS.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 // Override malloc() and free() to use the memory manager from FreeRTOS.
 // According to the documentation of libc, we also need to override
@@ -29,7 +31,7 @@ void __wrap_free(void* ptr) {
 }
 
 void* calloc(size_t num, size_t size) {
-  void *ptr = malloc(num * size);
+  void* ptr = malloc(num * size);
   if (ptr) {
     memset(ptr, 0, num * size);
   }
@@ -48,4 +50,49 @@ void* realloc(void* ptr, size_t newSize) {
 
 void* __wrap_realloc(void* ptr, size_t newSize) {
   return realloc(ptr, newSize);
+}
+
+// Implement functions required by libc as stubs
+// These functions aren't linked into the final binary
+
+__attribute__((error("stub"))) void _close(__attribute__((unused)) int fp) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) void _fstat(__attribute__((unused)) int fildes, __attribute__((unused)) struct stat* buf) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) pid_t _getpid() {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) int _isatty(__attribute__((unused)) int fd) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) int _kill(__attribute__((unused)) pid_t pid, __attribute__((unused)) int sig) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) off_t _lseek(__attribute__((unused)) int fd,
+                                            __attribute__((unused)) off_t offset,
+                                            __attribute__((unused)) int whence) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) ssize_t _read(__attribute__((unused)) int fd,
+                                             __attribute__((unused)) void* buf,
+                                             __attribute__((unused)) size_t count) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) ssize_t _write(__attribute__((unused)) int fd,
+                                              __attribute__((unused)) void* buf,
+                                              __attribute__((unused)) size_t count) {
+  __builtin_trap();
+}
+
+__attribute__((error("stub"))) void _exit(__attribute__((unused)) int status) {
+  __builtin_trap();
 }
